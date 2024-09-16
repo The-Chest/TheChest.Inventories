825221294ccd4ab435c98d638f08672cbbc43175
Franklin Bitencourt Dantas <franklin.bdantas@hotmail.com>
2024-09-16 12:00:25 -0300

    Adds Inventory Structure
    Adds Inventory Structure
    
    * Adds Inventory interfaces
    * Adds Inventory unit tests
    * Adds IInventorySlot and IInventory Factories
    * Adds IInventorySlot<T>.Contains method
    * Adds CreateRandom and CreateManyRandom in SlotItemFactory
    * Adds Example Inventory and slots
    * Changes ISlot<T>.Content to return T?
    * Changes the example folder structure
    * Changes global usings
    * Changes ISlotItemFactory.CreateItem to CreateDefault
    * Removes Base Inventory classes
    * Removes Enum and Event references from IInventoryStackSlot and  IInventorySlot<T>
    * Removes InventorySlotEventType
    
    Adds InventoryStackSlot
    
    Adds IInventoryStackSlot<T> experimental methods

---
+++ src/TheChest.Core.Inventories/Slots/Interfaces/IInventoryStackSlot.cs
@@ -0,0 +1,27 @@
+﻿using TheChest.Core.Slots.Interfaces;
+
+namespace TheChest.Core.Inventories.Slots.Interfaces
+{
+    /// <summary>
+    /// Interface with methods for a basic Inventory Stackable Slot 
+    /// </summary>
+    /// <typeparam name="T">Item the Slot Accept</typeparam>
+    public interface IInventoryStackSlot<T> : IStackSlot<T>
+    {
+        [Obsolete("Experimental")]
+        bool CanAdd(T item);
+        [Obsolete("Experimental")]
+        bool CanAdd(T[] items);
+
+        void Add(ref T[] items);
+
+        [Obsolete("Experimental")]
+        bool TryAdd(ref T[] items);
+
+        T[] Replace(ref T[] items);
+
+        T[] GetAmount(int amount);
+
+        T[] GetAll();
+    }
+}
