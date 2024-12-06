21438e16f8124bdf7c1d9597b7a3fc1d97ea551b
Franklin Bitencourt <Franklin.Bdantas@hotmail.com>
2024-12-06 18:24:44 -0300

    Adds IStackInventory unit tests bases

---
+++ tests/TheChest.Core.Inventories.Tests/Containers/Factories/Interfaces/IStackInventoryFactory.cs
@@ -0,0 +1,11 @@
+﻿using TheChest.Core.Inventories.Containers.Interfaces;
+
+namespace TheChest.Core.Inventories.Tests.Containers.Factories.Interfaces
+{
+    public interface IStackInventoryFactory<T> : IStackContainerFactory<T>
+    {
+        new IStackInventory<T> EmptyContainer(int size = 20);
+        new IStackInventory<T> FullContainer(int size, int stackSize, T item = default!);
+        new IStackInventory<T> ShuffledItemsContainer(int size, int stackSize, params T[] items);
+    }
+}
---
+++ tests/TheChest.Core.Inventories.Tests/Containers/StackInventory/StackInventoryTests.cs
@@ -0,0 +1,13 @@
+﻿using TheChest.Core.Tests.Containers;
+
+namespace TheChest.Core.Inventories.Tests.Containers
+{
+    public abstract partial class StackInventoryTests<T> : IStackContainerTests<T>
+    {
+        protected new readonly IStackInventoryFactory<T> containerFactory;
+        public StackInventoryTests(IStackInventoryFactory<T> containerFactory, ISlotItemFactory<T> itemFactory) : base(containerFactory, itemFactory) 
+        { 
+            this.containerFactory = containerFactory;
+        }
+    }
+}
