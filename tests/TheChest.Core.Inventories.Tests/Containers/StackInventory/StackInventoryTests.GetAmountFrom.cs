e38345b12d32984a7e54a3f959033ce78ab42153
Franklin Bitencourt <Franklin.Bdantas@hotmail.com>
2025-02-21 14:48:57 -0300

    Adds StackInventoryTests.GetAmountFrom

---
+++ tests/TheChest.Core.Inventories.Tests/Containers/StackInventory/StackInventoryTests.GetAmountFrom.cs
@@ -0,0 +1,83 @@
+﻿namespace TheChest.Core.Inventories.Tests.Containers
+{
+    public partial class StackInventoryTests<T>
+    {
+        [TestCase(-1)]
+        [TestCase(100)]
+        public void GetAmountFrom_InvalidIndex_ThrowsIndexOutOfRangeException(int index)
+        {
+            var inventory = this.containerFactory.EmptyContainer();
+            Assert.That(() => inventory.Get(index, 10), Throws.InstanceOf<IndexOutOfRangeException>());
+        }
+
+        [TestCase(-1)]
+        [TestCase(0)]
+        public void GetAmountFrom_InvalidAmount_ThrowsArgumentOutOfRangeException(int amount)
+        {
+            var inventory = this.containerFactory.EmptyContainer();
+            Assert.That(() => inventory.Get(0, amount), Throws.InstanceOf<ArgumentOutOfRangeException>());
+        }
+
+        [Test]
+        public void GetAmountFrom_EmptySlot_ReturnsEmptyArray()
+        {
+            var inventory = this.containerFactory.EmptyContainer();
+            var item = inventory.Get(0, 10);
+            Assert.That(item, Is.Empty);
+        }
+
+        [Test]
+        public void GetAmountFrom_SlotWithItems_ReturnsItems()
+        {
+            var index = this.random.Next(0, 20);
+            var stackSize = this.random.Next(10, 20);
+            var slotItem = this.itemFactory.CreateRandom();
+            var inventory = this.containerFactory.FullContainer(20, stackSize, slotItem);
+
+            var items = inventory.Get(index, 10);
+
+            Assert.That(items, Is.Not.Empty);
+            Assert.That(items.Count, Is.EqualTo(10));
+            Assert.That(items, Has.All.EqualTo(slotItem));
+        }
+
+        [Test]
+        public void GetAmountFrom_SlotWithItems_RemovesItemsFromSlot()
+        {
+            var index = this.random.Next(0, 20);
+            var stackSize = this.random.Next(10, 20);
+            var slotItem = this.itemFactory.CreateRandom();
+            var inventory = this.containerFactory.FullContainer(20, stackSize, slotItem);
+
+            inventory.Get(index, 10);
+
+            Assert.That(inventory[index].StackAmount, Is.EqualTo(stackSize - 10));
+        }
+
+        [Test]
+        public void GetAmountFrom_AmountBiggerThanItemsInsideSlot_ReturnsTheMaximumAmountPossible()
+        {
+            var index = this.random.Next(0, 20);
+            var stackSize = this.random.Next(1, 10);
+            var slotItem = this.itemFactory.CreateRandom();
+            var inventory = this.containerFactory.FullContainer(20, stackSize, slotItem);
+
+            var items = inventory.Get(index, 20);
+
+            Assert.That(items.Count, Is.EqualTo(10));
+        }
+
+        [Test]
+        public void GetAmountFrom_AmountBiggerThanItemsInsideSlot_RemovesAllItemsFromSlot()
+        {
+            var index = this.random.Next(0, 20);
+            var stackSize = this.random.Next(1, 10);
+            var slotItem = this.itemFactory.CreateRandom();
+            var inventory = this.containerFactory.FullContainer(20, stackSize, slotItem);
+
+            inventory.Get(index, 20);
+
+            Assert.That(inventory[index].StackAmount, Is.Zero);
+        }
+    }
+}
