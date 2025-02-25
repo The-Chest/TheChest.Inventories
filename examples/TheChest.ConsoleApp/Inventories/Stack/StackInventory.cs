a9e1def8945f1d1a16347ad685c1c4fdb8a1cf2a
Franklin Bitencourt <Franklin.Bdantas@hotmail.com>
2025-02-25 11:46:58 -0300

    Adds StackInventoryTests

---
+++ examples/TheChest.ConsoleApp/Inventories/Stack/StackInventory.cs
@@ -0,0 +1,10 @@
+﻿using TheChest.ConsoleApp.Items;
+using TheChest.Core.Inventories.Containers;
+
+namespace TheChest.ConsoleApp.Inventories.Stack
+{
+    public class StackInventory : StackInventory<Item>
+    {
+        public StackInventory(InventoryStackSlot[] slots) : base(slots) { }
+    }
+}
