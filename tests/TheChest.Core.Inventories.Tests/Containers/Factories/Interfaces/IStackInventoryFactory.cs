f8d73019fbcdb54a96273037b7933710940f3392
Franklin Bitencourt <Franklin.Bdantas@hotmail.com>
2025-03-13 15:12:08 -0300

    Fixes Factories' interfaces

--- tests/TheChest.Core.Inventories.Tests/Containers/Factories/Interfaces/IStackInventoryFactory.cs
+++ tests/TheChest.Core.Inventories.Tests/Containers/Factories/Interfaces/IStackInventoryFactory.cs
@@ -2,10 +2,10 @@
 
 namespace TheChest.Core.Inventories.Tests.Containers.Factories.Interfaces
 {
-    public interface IStackInventoryFactory<T> : IStackContainerFactory<T>
+    public interface IStackInventoryFactory<T>
     {
-        new IStackInventory<T> EmptyContainer(int size = 20);
-        new IStackInventory<T> FullContainer(int size, int stackSize, T item = default!);
-        new IStackInventory<T> ShuffledItemsContainer(int size, int stackSize, params T[] items);
+        IStackInventory<T> EmptyContainer(int size = 20);
+        IStackInventory<T> FullContainer(int size, int stackSize, T item = default!);
+        IStackInventory<T> ShuffledItemsContainer(int size, int stackSize, params T[] items);
     }
 }
--- tests/TheChest.Core.Inventories.Tests/Slots/Factories/Interfaces/IInventorySlotFactory.cs
+++ tests/TheChest.Core.Inventories.Tests/Slots/Factories/Interfaces/IInventorySlotFactory.cs
@@ -6,18 +6,18 @@ namespace TheChest.Core.Inventories.Tests.Slots.Factories.Interfaces
     /// Factory interface to instantiate any <see cref="IInventorySlot{T}"/>
     /// </summary>
     /// <typeparam name="T">Any type of item inside IInventorySlot</typeparam>
-    public interface IInventorySlotFactory<T> : ISlotFactory<T>
+    public interface IInventorySlotFactory<T>
     {
         /// <summary>
         /// Creates an <see cref="IInventorySlot{T}"/> with no item inside it
         /// </summary>
         /// <returns>An empty <see cref="IInventorySlot{T}"/></returns>
-        new IInventorySlot<T> EmptySlot();
+        IInventorySlot<T> EmptySlot();
         /// <summary>
         /// Creates an <see cref="IInventorySlot{T}"/> with an item inside it
         /// </summary>
         /// <param name="item">The item that will be inside the created <see cref="IInventorySlot{T}"/></param>
         /// <returns>A full <see cref="IInventorySlot{T}"/></returns>
-        new IInventorySlot<T> FullSlot(T item);
+        IInventorySlot<T> FullSlot(T item);
     }
 }
