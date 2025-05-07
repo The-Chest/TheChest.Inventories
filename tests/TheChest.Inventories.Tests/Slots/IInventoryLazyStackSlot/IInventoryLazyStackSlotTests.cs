2dcf9bf6e9a7c398d58ae94aba5c10eff2fffd56
Franklin Bitencourt <Franklin.Bdantas@hotmail.com>
2025-05-07 10:42:21 -0300

    Adds IInventoryLazyStackSlotTests<T>

---
+++ tests/TheChest.Inventories.Tests/Slots/IInventoryLazyStackSlot/IInventoryLazyStackSlotTests.cs
@@ -0,0 +1,18 @@
+﻿namespace TheChest.Inventories.Tests.Slots.IInventoryLazyStackSlot
+{
+    public abstract partial class IInventoryLazyStackSlotTests<T>
+    {
+        protected readonly IInventoryStackSlotFactory<T> slotFactory;
+        protected readonly ISlotItemFactory<T> itemFactory;
+
+        protected readonly Random random;
+
+        public IInventoryLazyStackSlotTests(IInventoryStackSlotFactory<T> slotFactory, ISlotItemFactory<T> itemFactory)
+        {
+            this.slotFactory = slotFactory;
+            this.itemFactory = itemFactory;
+
+            this.random = new Random();
+        }
+    }
+}
