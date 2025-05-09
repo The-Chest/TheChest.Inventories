f7e0f84bc83efe283422d69384c13fa1296c132d
Franklin Bitencourt <Franklin.Bdantas@hotmail.com>
2025-05-09 10:09:21 -0300

    Adds LazyStackInventoryTests.GetByIndex

---
+++ tests/TheChest.Inventories.Tests/Containers/LazyStackInventory/LazyStackInventoryTests.get_by_index.cs
@@ -0,0 +1,36 @@
+﻿namespace TheChest.Inventories.Tests.Containers.LazyStackInventory
+{
+    public partial class LazyStackInventoryTests<T>
+    {
+        [Test]
+        public void Get_ByIndex_ValidIndex_ReturnsItem()
+        {
+            var expectedItem = this.itemFactory.CreateDefault();
+            var inventory = this.containerFactory.FullContainer(20, 1, expectedItem);
+
+            var result = inventory.Get(0);
+
+            Assert.That(result, Is.EqualTo(expectedItem));
+        }
+
+        [Test]
+        public void Get_ByIndex_EmptySlot_ReturnsNull()
+        {
+            var inventory = this.containerFactory.EmptyContainer(20);
+
+            var result = inventory.Get(0);
+
+            Assert.That(result, Is.Null);
+        }
+
+        [TestCase(-1)]
+        [TestCase(1000)]
+        public void Get_ByIndex_ShouldThrowArgumentOutOfRangeException_WhenIndexIsInvalid(int index)
+        {
+            var item = this.itemFactory.CreateDefault();
+            var inventory = this.containerFactory.FullContainer(20, 1, item);
+
+            Assert.Throws<ArgumentOutOfRangeException>(() => inventory.Get(index));
+        }
+    }
+}
