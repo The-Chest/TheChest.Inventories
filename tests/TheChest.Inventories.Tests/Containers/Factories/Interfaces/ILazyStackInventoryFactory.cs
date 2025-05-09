8f2b2fd439a0370b60a062f875d91c289b1b4bf4
Franklin Bitencourt <Franklin.Bdantas@hotmail.com>
2025-05-09 08:05:26 -0300

    Adds ILazyStackInventoryFactory

---
+++ tests/TheChest.Inventories.Tests/Containers/Factories/Interfaces/ILazyStackInventoryFactory.cs
@@ -0,0 +1,11 @@
+﻿using TheChest.Inventories.Containers.Interfaces;
+
+namespace TheChest.Inventories.Tests.Containers.Factories.Interfaces
+{
+    public interface ILazyStackInventoryFactory<T>
+    {
+        ILazyStackInventory<T> EmptyContainer(int size = 20);
+        ILazyStackInventory<T> FullContainer(int size, int stackSize, T item);
+        ILazyStackInventory<T> ShuffledItemsContainer(int size, int stackSize, T item);
+    }
+}
