9cf861fa8c25a69cd1f97c425d07b83e1e040b8a
Franklin Bitencourt <Franklin.Bdantas@hotmail.com>
2025-05-11 18:02:39 -0300

    Adds LazyStackInventoryTests.GetAllByItem

---
+++ tests/TheChest.Inventories.Tests/Containers/LazyStackInventory/LazyStackInventoryTests.GetAllByItem.cs
@@ -0,0 +1,40 @@
+﻿namespace TheChest.Inventories.Tests.Containers.LazyStackInventory
+{
+    public partial class LazyStackInventoryTests<T>
+    {
+        [Test]
+        public void GetAll_ByItem_NullItem_ThrowsArgumentNullException()
+        {
+            var inventory = this.containerFactory.EmptyContainer();
+
+            Assert.Throws<ArgumentNullException>(() => inventory.GetAll(item: default!));
+        }
+
+        [Test]
+        public void GetAll_ByItem_ExistingItem_ReturnsAllMatchingItems()
+        {
+            var inventoryItem = this.itemFactory.CreateDefault();
+            var inventoryRandomItem = this.itemFactory.CreateRandom();
+            var inventory = this.containerFactory.ShuffledItemsContainer(10, 5, inventoryItem, inventoryRandomItem);
+
+            var item = this.itemFactory.CreateDefault();
+            var result = inventory.GetAll(item);
+
+            Assert.That(result, Is.Not.Empty);
+            Assert.That(result, Has.All.EqualTo(item));
+        }
+
+        [Test]
+        public void GetAll_ByItem_ExistingItem_ReMovesAllMatchingItemsFromInventory()
+        {
+            var inventoryItem = this.itemFactory.CreateDefault();
+            var inventoryRandomItem = this.itemFactory.CreateRandom();
+            var inventory = this.containerFactory.ShuffledItemsContainer(10, 5, inventoryItem, inventoryRandomItem);
+
+            var item = this.itemFactory.CreateDefault();
+            inventory.GetAll(item);
+
+            Assert.That(inventory.Slots, Has.None.EqualTo(item));
+        }
+    }
+}
