299041ab895998c7902850ec894fd54b32083932
Franklin Bitencourt <Franklin.Bdantas@hotmail.com>
2025-05-11 18:54:03 -0300

    Removes replace optional param from LazyStackInventory.AddAt

--- src/TheChest.Inventories/Containers/LazyStackInventory.cs
+++ src/TheChest.Inventories/Containers/LazyStackInventory.cs
@@ -76,7 +76,7 @@ namespace TheChest.Inventories.Containers
         /// <inheritdoc/>
         /// <exception cref="ArgumentNullException">When <paramref name="item"/> is null</exception>
         /// <exception cref="ArgumentOutOfRangeException">When <paramref name="amount"/> is zero or smaller or <paramref name="index"/> is bigger than <see cref="StackContainer{T}.Size"/> or smaller than zero</exception>
-        public virtual T[] AddAt(T item, int index, int amount, bool replace = true)
+        public virtual T[] AddAt(T item, int index, int amount, bool replace)
         {
             if (item is null)
                 throw new ArgumentNullException(nameof(item));
