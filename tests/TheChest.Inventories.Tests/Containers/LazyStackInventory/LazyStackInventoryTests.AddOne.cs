02aef05c344eb95692643737bd3fac6faf5ecd4f
Franklin Bitencourt <Franklin.Bdantas@hotmail.com>
2025-05-15 00:05:06 -0300

    Adds ILazyStackInventory.Add tests

---
+++ tests/TheChest.Inventories.Tests/Containers/LazyStackInventory/LazyStackInventoryTests.AddOne.cs
@@ -0,0 +1,73 @@
+﻿using TheChest.Core.Slots.Interfaces;
+
+namespace TheChest.Inventories.Tests.Containers.LazyStackInventory
+{
+    public partial class LazyStackInventoryTests<T>
+    {
+        [Test]
+        public void Add_NullItem_ThrowsArgumentNullException()
+        {
+            var inventory = this.containerFactory.EmptyContainer();
+
+            Assert.Throws<ArgumentNullException>(() => inventory.Add(default!));
+        }
+
+        [Test]
+        public void Add_SuccessfullyAdded_ReturnsTrue()
+        {
+            var inventory = this.containerFactory.EmptyContainer();
+
+            var item = this.itemFactory.CreateDefault();
+            var result = inventory.Add(item);
+
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void Add_EmptyInventory_AddsToFirstAvilableSlot()
+        {
+            var inventory = this.containerFactory.EmptyContainer();
+
+            var item = this.itemFactory.CreateDefault();
+            inventory.Add(item);
+            
+            Assert.Multiple(() =>
+            {
+                Assert.That(inventory[0].Content, Has.All.EqualTo(item));
+                Assert.That(inventory[0].IsEmpty, Is.False);
+                Assert.That(inventory[0].StackAmount, Is.EqualTo(1));
+            });
+        }
+
+        [Test]
+        public void Add_FailedToAdd_ReturnsFalse()
+        {
+            var items = this.itemFactory.CreateRandom();
+            var inventory = this.containerFactory.FullContainer(10,5, items);
+
+            var item = this.itemFactory.CreateDefault();
+            var result = inventory.Add(item);
+
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public void Add_EmptyInventory_DoesNotAddsToInventory()
+        {
+            var items = this.itemFactory.CreateRandom();
+            var inventory = this.containerFactory.FullContainer(10, 5, items);
+
+            var item = this.itemFactory.CreateDefault();
+            inventory.Add(item);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(inventory.Slots, 
+                    Has.All.Matches<IStackSlot<T>>(
+                        slot => slot.IsFull && slot.Content!.All(item => item.Equals(items))
+                    )
+                );
+            });
+        }
+    }
+}
