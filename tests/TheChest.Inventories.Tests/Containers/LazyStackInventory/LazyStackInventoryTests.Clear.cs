934eb575c4a19de348cffc9df4b7a8db5fe00552
Franklin Bitencourt <Franklin.Bdantas@hotmail.com>
2025-05-18 14:34:45 -0300

    Adds LazyStackInventoryTests.Clear

---
+++ tests/TheChest.Inventories.Tests/Containers/LazyStackInventory/LazyStackInventoryTests.Clear.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework.Internal;
+using TheChest.Core.Slots.Interfaces;
+
+namespace TheChest.Inventories.Tests.Containers.LazyStackInventory
+{
+    public partial class LazyStackInventoryTests<T>
+    {
+        [Test]
+        public void Clear_EmptyInventory_ReturnsEmptyArray()
+        {
+            var inventory = this.containerFactory.EmptyContainer();
+
+            var result = inventory.Clear();
+
+            Assert.That(result, Is.Empty);
+        }
+
+        [Test]
+        public void Clear_InventoryWithItems_ReturnsAllItems()
+        {
+            var size = this.random.Next(1, 20);
+            var stackSize = this.random.Next(1, 10);
+            var item = this.itemFactory.CreateDefault();
+            var inventory = this.containerFactory.FullContainer(size, stackSize, item);
+
+            var result = inventory.Clear();
+
+            Assert.That(result, Has.All.EqualTo(item).And.Length.EqualTo(size * stackSize));
+        }
+
+        [Test]
+        public void Clear_InventoryWithItems_EmptiesSlots()
+        {
+            var size = this.random.Next(1, 20);
+            var stackSize = this.random.Next(1, 10);
+            var item = this.itemFactory.CreateDefault();
+            var inventory = this.containerFactory.FullContainer(size, stackSize, item);
+
+            inventory.Clear();
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(inventory.Slots,
+                    Has.All.Matches<IStackSlot<T>>(
+                        slot => slot.Content!.Length == 0 &&  slot.StackAmount == 0
+                    )
+                );
+                Assert.That(inventory.IsEmpty, Is.True);
+            });
+        }
+    }
+}
