e3f1c3d56a33be1edcff1c8309867a1f23c9acc0
Franklin Bitencourt <Franklin.Bdantas@hotmail.com>
2025-05-24 20:45:39 -0300

    Adds IInventoryStackSlotTests.replace_one

---
+++ tests/TheChest.Inventories.Tests/Slots/IInventoryStackSlot/IInventoryStackSlotTests.replace_one.cs
@@ -0,0 +1,94 @@
+﻿namespace TheChest.Inventories.Tests.Slots
+{
+    public partial class IInventoryStackSlotTests<T>
+    {
+        [Test]
+        public void ReplaceOne_NullItemReplace_ThrowsArgumentNullException()
+        {
+            var slotItems = this.itemFactory.CreateMany(10);
+            var slot = this.slotFactory.FullSlot(slotItems);
+
+            var item = default(T);
+
+            Assert.That(() => slot.Replace(ref item!), Throws.ArgumentNullException);
+        }
+
+        [Test]
+        public void ReplaceOne_EmptySlot_AddsItem()
+        {
+            var slot = this.slotFactory.EmptySlot();
+
+            var item = this.itemFactory.CreateDefault();
+            var expectedResult = new T[1];
+            Array.Fill(expectedResult, item);
+            slot.Replace(ref item);
+
+            Assert.That(slot.Content, Has.Length.EqualTo(1).And.EqualTo(expectedResult));
+        }
+
+        [Test]
+        public void ReplaceOne_EmptySlot_ReturnsEmptyArray()
+        {
+            var slot = this.slotFactory.EmptySlot();
+
+            var item = this.itemFactory.CreateDefault();
+            var result = slot.Replace(ref item);
+
+            Assert.That(result, Is.Empty);
+        }
+
+        [Test]
+        public void ReplaceOne_ItemDifferentFromSlot_ReturnsItemsFromSlot()
+        {
+            var items = this.itemFactory.CreateMany(5);
+            var slot = this.slotFactory.WithItems(items);
+
+            var replacingItem = this.itemFactory.CreateRandom();
+            var result = slot.Replace(ref replacingItem);
+
+            Assert.That(result, Is.EqualTo(items));
+        }
+
+        [Test]
+        public void ReplaceOne_ItemDifferentFromSlot_AddsItemsToSlot()
+        {
+            var items = this.itemFactory.CreateMany(5);
+            var slot = this.slotFactory.WithItems(items);
+
+            var replacingItem = this.itemFactory.CreateRandom();
+            var expectedResult = new T[1];
+            Array.Fill(expectedResult, replacingItem);
+
+            slot.Replace(ref replacingItem);
+
+            Assert.That(slot.Content, Is.EqualTo(expectedResult));
+        }
+
+        [Test]
+        public void ReplaceOne_ItemEqualToSlot_ReturnsItemsFromSlot()
+        {
+            var items = this.itemFactory.CreateMany(5);
+            var slot = this.slotFactory.WithItems(items);
+
+            var replacingItem = this.itemFactory.CreateDefault();
+            var result = slot.Replace(ref replacingItem);
+
+            Assert.That(result, Is.EqualTo(items));
+        }
+
+        [Test]
+        public void ReplaceOne_ItemEqualToSlot_AddsItemsToSlot()
+        {
+            var items = this.itemFactory.CreateMany(5);
+            var slot = this.slotFactory.WithItems(items);
+
+            var replacingItem = this.itemFactory.CreateDefault();
+            var expectedResult = new T[1];
+            Array.Fill(expectedResult, replacingItem);
+
+            slot.Replace(ref replacingItem);
+
+            Assert.That(slot.Content, Is.EqualTo(expectedResult));
+        }
+    }
+}
