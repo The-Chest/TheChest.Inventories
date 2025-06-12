b738148f21bc9b3169fa4adf44da1d581cca703e
Franklin Bitencourt <Franklin.Bdantas@hotmail.com>
2025-06-12 00:52:13 -0300

    Changes trigger to fires events in docs

--- src/TheChest.Inventories/Containers/Inventory.cs
+++ src/TheChest.Inventories/Containers/Inventory.cs
@@ -37,7 +37,7 @@ namespace TheChest.Inventories.Containers
         /// </summary>
         /// <param name="items">Array of items to be added to any avaliable slot found</param>
         /// <remarks>
-        /// The method triggers <see cref="OnAdd"/> event after every possible <paramref name="items"/> is added. 
+        /// The method fires <see cref="OnAdd"/> event after every possible <paramref name="items"/> is added. 
         /// </remarks>
         /// <returns>
         /// An array of <paramref name="items"/> that were not added to the inventory.
@@ -83,7 +83,7 @@ namespace TheChest.Inventories.Containers
 
         /// <inheritdoc/>
         /// <remarks>
-        /// The method triggers <see cref="OnAdd"/> event when <paramref name="item"/> is added.
+        /// The method fires <see cref="OnAdd"/> event when <paramref name="item"/> is added.
         /// </remarks>
         /// <exception cref="ArgumentNullException">When <paramref name="item"/> is null</exception>
         public virtual bool Add(T item)
@@ -106,7 +106,7 @@ namespace TheChest.Inventories.Containers
 
         /// <inheritdoc/>
         /// <remarks>
-        /// The method triggers <see cref="OnAdd"/> event when <paramref name="item"/> is added on <paramref name="index"/>.
+        /// The method fires <see cref="OnAdd"/> event when <paramref name="item"/> is added on <paramref name="index"/>.
         /// </remarks>
         /// <exception cref="ArgumentNullException">When <paramref name="item"/> is null</exception>
         /// <exception cref="ArgumentOutOfRangeException">When <paramref name="index"/> is smaller than zero or bigger than <see cref="Inventory{T}.Size"/></exception>
@@ -137,7 +137,7 @@ namespace TheChest.Inventories.Containers
 
         /// <inheritdoc/>
         /// <remarks>
-        /// The method triggers the <see cref="OnGet"/> event with every item returned from it.
+        /// The method fires the <see cref="OnGet"/> event with every item returned from it.
         /// </remarks>
         public virtual T[] Clear()
         {
@@ -161,7 +161,7 @@ namespace TheChest.Inventories.Containers
 
         /// <inheritdoc/>
         /// <remarks>
-        /// The method triggers the <see cref="OnGet"/> event when any amount of <paramref name="item"/> is found.
+        /// The method fires the <see cref="OnGet"/> event when any amount of <paramref name="item"/> is found.
         /// </remarks>
         /// <exception cref="ArgumentNullException">When <paramref name="item"/> is null</exception>
         public virtual T[] GetAll(T item)
@@ -188,7 +188,7 @@ namespace TheChest.Inventories.Containers
 
         /// <inheritdoc/>
         /// <remarks>
-        /// The method triggers the <see cref="OnGet"/> event if an item is found on <paramref name="index"/>.
+        /// The method fires the <see cref="OnGet"/> event if an item is found on <paramref name="index"/>.
         /// </remarks>
         /// <exception cref="ArgumentOutOfRangeException">When <paramref name="index"/> is smaller than zero or bigger than <see cref="Inventory{T}.Size"/></exception>
         public virtual T? Get(int index)
@@ -206,7 +206,7 @@ namespace TheChest.Inventories.Containers
 
         /// <inheritdoc/>
         /// <remarks>
-        /// The method triggers the <see cref="OnGet"/> event when <paramref name="item"/> is found.
+        /// The method fires the <see cref="OnGet"/> event when <paramref name="item"/> is found.
         /// </remarks>
         /// <exception cref="ArgumentNullException">When <paramref name="item"/> is null</exception>
         public virtual T? Get(T item)
@@ -228,7 +228,7 @@ namespace TheChest.Inventories.Containers
 
         /// <inheritdoc/>
         /// <remarks>
-        /// The method triggers the <see cref="OnGet"/> event when the maximum possible <paramref name="amount"/> of <paramref name="item"/> is found.
+        /// The method fires the <see cref="OnGet"/> event when the maximum possible <paramref name="amount"/> of <paramref name="item"/> is found.
         /// </remarks>
         /// <exception cref="ArgumentNullException">When <paramref name="item"/> is null</exception>
         /// <exception cref="ArgumentOutOfRangeException">When <paramref name="amount"/> is zero or smaller</exception>
@@ -282,7 +282,7 @@ namespace TheChest.Inventories.Containers
 
         /// <inheritdoc/>
         /// <remarks>
-        /// The method triggers the <see cref="OnMove"/> event.
+        /// The method fires the <see cref="OnMove"/> event.
         /// </remarks>
         /// <exception cref="ArgumentOutOfRangeException">When <paramref name="origin"/> or <paramref name="target"/> are smaller than zero or bigger than the container size</exception>
         public virtual void Move(int origin, int target)
