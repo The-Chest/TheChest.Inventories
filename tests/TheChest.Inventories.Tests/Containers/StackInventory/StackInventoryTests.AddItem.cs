d5452d98fba0d5cbac93ef9f0d47b2b08ad81aa2
Franklin Bitencourt <Franklin.Bdantas@hotmail.com>
2025-06-21 00:18:36 -0300

    Adds StackInventoryTests.AddItem OnAddEvent

--- tests/TheChest.Inventories.Tests/Containers/StackInventory/StackInventoryTests.AddItem.cs
+++ tests/TheChest.Inventories.Tests/Containers/StackInventory/StackInventoryTests.AddItem.cs
@@ -5,7 +5,7 @@ namespace TheChest.Inventories.Tests.Containers
     public partial class StackInventoryTests<T>
     {
         [Test]
-        public void Add_EmptyInventory_AddsToFirstEmptySlot()
+        public void AddItem_EmptyInventory_AddsToFirstEmptySlot()
         {
             var item = this.itemFactory.CreateDefault();
             var inventory = this.containerFactory.EmptyContainer();
@@ -20,7 +20,7 @@ namespace TheChest.Inventories.Tests.Containers
         }
 
         [Test]
-        public void Add_EmptyInventory_CallsOnAddEvent()
+        public void AddItem_EmptyInventory_CallsOnAddEvent()
         {
             var item = this.itemFactory.CreateDefault();
             var inventory = this.containerFactory.EmptyContainer();
@@ -38,7 +38,7 @@ namespace TheChest.Inventories.Tests.Containers
         }
 
         [Test]
-        public void Add_EmptyInventory_ReturnsTrue()
+        public void AddItem_EmptyInventory_ReturnsTrue()
         {
             var item = this.itemFactory.CreateDefault();
             var inventory = this.containerFactory.EmptyContainer();
@@ -49,7 +49,7 @@ namespace TheChest.Inventories.Tests.Containers
         }
 
         [Test]
-        public void Add_InventoryWithItems_AddsToAvailableSlot()
+        public void AddItem_InventoryWithItems_AddsToAvailableSlot()
         {
             var item = this.itemFactory.CreateDefault();
             var inventory = this.containerFactory.ShuffledItemsContainer(20, 10, this.itemFactory.CreateManyRandom(10));
@@ -60,7 +60,7 @@ namespace TheChest.Inventories.Tests.Containers
         }
 
         [Test]
-        public void Add_InventoryWithItems_CallsOnAddEvent()
+        public void AddItem_InventoryWithItems_CallsOnAddEvent()
         {
             var item = this.itemFactory.CreateDefault();
             var inventory = this.containerFactory.ShuffledItemsContainer(20, 10, this.itemFactory.CreateManyRandom(10));
@@ -81,7 +81,7 @@ namespace TheChest.Inventories.Tests.Containers
         }
 
         [Test]
-        public void Add_InventoryWithSameItem_AddsToAvailableSlotWithSameItem()
+        public void AddItem_InventoryWithSameItem_AddsToAvailableSlotWithSameItem()
         {
             var item = this.itemFactory.CreateDefault();
             var amount = this.random.Next(2, 10);
@@ -94,7 +94,7 @@ namespace TheChest.Inventories.Tests.Containers
         }
 
         [Test]
-        public void Add_InventoryWithFullSlotWithSameItem_AddsToFirstAvailableSlot()
+        public void AddItem_InventoryWithFullSlotWithSameItem_AddsToFirstAvailableSlot()
         {
             var item = this.itemFactory.CreateDefault();
             var amount = this.random.Next(2, 10);
@@ -110,7 +110,7 @@ namespace TheChest.Inventories.Tests.Containers
         }
 
         [Test]
-        public void Add_InventoryWithSameItem_AddsToSlotWithItem()
+        public void AddItem_InventoryWithSameItem_AddsToSlotWithItem()
         {
             var item = this.itemFactory.CreateDefault();
             var items = this.itemFactory.CreateManyRandom(19)
@@ -130,7 +130,7 @@ namespace TheChest.Inventories.Tests.Containers
         }
 
         [Test]
-        public void Add_FullInventory_DoesNotAddToSlot()
+        public void AddItem_FullInventory_DoesNotAddToSlot()
         {
             var item = this.itemFactory.CreateDefault();
             var inventory = this.containerFactory.FullContainer(10,10, this.itemFactory.CreateRandom());
@@ -141,7 +141,7 @@ namespace TheChest.Inventories.Tests.Containers
         }
 
         [Test]
-        public void Add_FullInventory_DoesNotCallOnAddEvent()
+        public void AddItem_FullInventory_DoesNotCallOnAddEvent()
         {
             var item = this.itemFactory.CreateDefault();
             var inventory = this.containerFactory.FullContainer(10, 10, this.itemFactory.CreateRandom());
@@ -150,7 +150,7 @@ namespace TheChest.Inventories.Tests.Containers
         }
 
         [Test]
-        public void Add_FullInventory_ReturnsFalse()
+        public void AddItem_FullInventory_ReturnsFalse()
         {
             var item = this.itemFactory.CreateDefault();
             var inventory = this.containerFactory.FullContainer(10, 10, this.itemFactory.CreateRandom());
