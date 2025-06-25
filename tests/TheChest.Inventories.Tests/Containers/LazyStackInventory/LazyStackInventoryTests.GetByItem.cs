47f1b37ace2202fd83158d4f4f9b2a2ce6cad5ce
Franklin Bitencourt <Franklin.Bdantas@hotmail.com>
2025-06-24 23:57:40 -0300

    Adds LazyStackInventoryTests.GetByItem OnGet event unit tests

--- tests/TheChest.Inventories.Tests/Containers/LazyStackInventory/LazyStackInventoryTests.GetByItem.cs
+++ tests/TheChest.Inventories.Tests/Containers/LazyStackInventory/LazyStackInventoryTests.GetByItem.cs
@@ -35,6 +35,27 @@
         }
 
         [Test]
+        public void Get_ByItem_ExistingItem_CallsOnGetEvent()
+        {
+            var items = this.itemFactory.CreateDefault();
+            var amount = 5;
+            var inventory = this.containerFactory.FullContainer(10, amount, items);
+            var expectedItem = this.itemFactory.CreateDefault();
+
+            inventory.OnGet += (sender, args) => {
+                Assert.That(args.Data, Has.Count.EqualTo(1));
+                var firstEvent = args.Data.First();
+                Assert.Multiple(() =>
+                {
+                    Assert.That(firstEvent.Item, Is.EqualTo(expectedItem));
+                    Assert.That(firstEvent.Index, Is.EqualTo(0));
+                    Assert.That(firstEvent.Amount, Is.EqualTo(1));
+                });
+            };
+            inventory.Get(expectedItem);
+        }
+
+        [Test]
         public void Get_ByItem_NotFoundItem_ReturnsNull()
         {
             var items = this.itemFactory.CreateDefault();
@@ -44,6 +65,17 @@
             var result = inventory.Get(item);
 
             Assert.That(result, Is.Null);
+        }
+
+        [Test]
+        public void Get_ByItem_NotFoundItem_DoesNotCallOnGetEvent()
+        {
+            var items = this.itemFactory.CreateDefault();
+            var inventory = this.containerFactory.ShuffledItemsContainer(10, 5, items);
+            var item = this.itemFactory.CreateRandom();
+
+            inventory.OnGet += (sender, args) => Assert.Fail("OnGet event should not be called when an item that is not found.");
+            inventory.Get(item);
         }
     }
 }
