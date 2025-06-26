16690f537e46663d25a669fd37333e6e44743088
Franklin Bitencourt <Franklin.Bdantas@hotmail.com>
2025-06-25 21:53:37 -0300

    Adds LazyStackInventoryTests.AddAmount OnAdd event

--- tests/TheChest.Inventories.Tests/Containers/LazyStackInventory/LazyStackInventoryTests.AddAmount.cs
+++ tests/TheChest.Inventories.Tests/Containers/LazyStackInventory/LazyStackInventoryTests.AddAmount.cs
@@ -40,6 +40,26 @@ namespace TheChest.Inventories.Tests.Containers.LazyStackInventory
         }
 
         [Test]
+        public void Add_WithAmount_EmptyInventory_CallsOnAddEvent()
+        {
+            var inventory = this.containerFactory.EmptyContainer();
+
+            var item = this.itemFactory.CreateDefault();
+            var amount = this.random.Next(1, 5);
+            inventory.OnAdd += (sender, args) => {
+                Assert.That(args.Data, Has.Count.EqualTo(1));
+                Assert.Multiple(() =>
+                {
+                    var firstEvent = args.Data.FirstOrDefault();
+                    Assert.That(firstEvent.Item, Is.EqualTo(item));
+                    Assert.That(firstEvent.Index, Is.EqualTo(0));
+                    Assert.That(firstEvent.Amount, Is.EqualTo(amount));
+                });
+            };
+            inventory.Add(item, amount);
+        }
+
+        [Test]
         public void Add_WithAmount_SuccessfullyAddedItems_ReturnsZero()
         {
             var inventory = this.containerFactory.EmptyContainer();
@@ -64,6 +84,20 @@ namespace TheChest.Inventories.Tests.Containers.LazyStackInventory
             var result = inventory.Add(item, amount);
 
             Assert.That(result, Is.EqualTo(amount));
+        }
+
+        [Test]
+        public void Add_WithAmount_FullInventory_DoesNotCallOnAddEvent()
+        {
+            var size = this.random.Next(1, 20);
+            var stackSize = this.random.Next(1, 10);
+            var randomItem = this.itemFactory.CreateRandom();
+            var inventory = this.containerFactory.FullContainer(size, stackSize, randomItem);
+
+            var item = this.itemFactory.CreateDefault();
+            var amount = this.random.Next(1, 5);
+            inventory.OnAdd += (sender, args) => Assert.Fail("OnAdd should not be called when inventory is full.");
+            inventory.Add(item, amount);
         }
 
         [Test]
@@ -101,6 +135,30 @@ namespace TheChest.Inventories.Tests.Containers.LazyStackInventory
             var result = inventory.Add(item, amount);
 
             Assert.That(result, Is.EqualTo(amount - stackSize));
+        }
+
+        [Test]
+        public void Add_WithAmount_NotAllItemsBeAdded_CallsOnAddEvent()
+        {
+            var size = this.random.Next(2, 20);
+            var stackSize = this.random.Next(2, 10);
+            var randomItem = this.itemFactory.CreateManyRandom(size - 1);
+            var inventory = this.containerFactory.ShuffledItemsContainer(size, stackSize, randomItem);
+
+            var item = this.itemFactory.CreateDefault();
+            var amount = stackSize + this.random.Next(1, 5);
+            inventory.OnAdd += (sender, args) =>
+            {
+                Assert.That(args.Data, Has.Count.EqualTo(1));
+                Assert.Multiple(() =>
+                {
+                    var firstEvent = args.Data.FirstOrDefault();
+                    Assert.That(firstEvent.Item, Is.EqualTo(item));
+                    Assert.That(firstEvent.Index, Is.InRange(0, size));
+                    Assert.That(firstEvent.Amount, Is.EqualTo(stackSize));
+                });
+            };
+            inventory.Add(item, amount);
         }
 
         [Test]
