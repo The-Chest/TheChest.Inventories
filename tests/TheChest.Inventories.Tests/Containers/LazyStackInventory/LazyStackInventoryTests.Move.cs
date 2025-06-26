212fd71140306fb171993ea4f85ff448987bc695
Franklin Bitencourt <Franklin.Bdantas@hotmail.com>
2025-06-25 22:54:45 -0300

    Adds LazyStackInventoryTests.Move OnMove event

--- tests/TheChest.Inventories.Tests/Containers/LazyStackInventory/LazyStackInventoryTests.Move.cs
+++ tests/TheChest.Inventories.Tests/Containers/LazyStackInventory/LazyStackInventoryTests.Move.cs
@@ -54,6 +54,40 @@
         }
 
         [Test]
+        public void Move_BothSlotsWithItems_CallsOnMove()
+        {
+            var size = this.random.Next(2, 20);
+            var stackSize = this.random.Next(1, 10);
+            var items = this.itemFactory.CreateManyRandom(size);
+            var inventory = this.containerFactory.ShuffledItemsContainer(size, stackSize, items);
+
+            var origin = 0;
+            var target = 1;
+            var itemFromOrigin = inventory[origin].Content!.FirstOrDefault();
+            var ItemFromTarget = inventory[target].Content!.FirstOrDefault();
+            inventory.OnMove += (o, args) =>
+            {
+                Assert.That(args.Data, Has.Count.EqualTo(2));
+                Assert.Multiple(() =>
+                {
+                    var firstEvent = args.Data.FirstOrDefault();
+                    Assert.That(firstEvent.Item, Is.EqualTo(itemFromOrigin));
+                    Assert.That(firstEvent.FromIndex, Is.EqualTo(origin));
+                    Assert.That(firstEvent.ToIndex, Is.EqualTo(target));
+                });
+
+                Assert.Multiple(() =>
+                {
+                    var secondEvent = args.Data.Skip(1).FirstOrDefault();
+                    Assert.That(secondEvent.Item, Is.EqualTo(ItemFromTarget));
+                    Assert.That(secondEvent.FromIndex, Is.EqualTo(target));
+                    Assert.That(secondEvent.ToIndex, Is.EqualTo(origin));
+                });
+            };
+            inventory.Move(origin, target);
+        }
+
+        [Test]
         public void Move_EmptyTarget_MovesItem()
         {
             var stackSize = this.random.Next(1, 10);
@@ -76,15 +110,41 @@
         }
 
         [Test]
+        public void Move_EmptyTarget_CallsOnMoveEventOnlyWithOrigin()
+        {
+            var stackSize = this.random.Next(1, 10);
+            var inventory = this.containerFactory.EmptyContainer(2, stackSize);
+
+            var item = this.itemFactory.CreateRandom();
+            var originIndex = 0;
+            var targetIndex = 1;
+            inventory.Add(item);
+            inventory.OnMove += (o, args) =>
+            {
+                Assert.That(args.Data, Has.Count.EqualTo(1));
+                Assert.Multiple(() =>
+                {
+                    var firstEvent = args.Data.FirstOrDefault();
+                    Assert.That(firstEvent.Item, Is.EqualTo(item));
+                    Assert.That(firstEvent.FromIndex, Is.EqualTo(originIndex));
+                    Assert.That(firstEvent.ToIndex, Is.EqualTo(targetIndex));
+                });
+            };
+            inventory.Move(originIndex, targetIndex);
+        }
+
+        [Test]
         public void Move_EmptyOrigin_MovesItem()
         {
             var stackSize = this.random.Next(1, 10);
             var inventory = this.containerFactory.EmptyContainer(2, stackSize);
 
             var item = this.itemFactory.CreateRandom();
-            inventory.AddAt(item, 1, 1);
+            var originIndex = 0;
+            var targetIndex = 1;
+            inventory.AddAt(item, targetIndex, 1);
 
-            inventory.Move(0, 1);
+            inventory.Move(originIndex, targetIndex);
 
             Assert.Multiple(() => {
                 Assert.That(inventory[0].Content, Has.All.EqualTo(item));
@@ -95,6 +155,31 @@
                 Assert.That(inventory[1].Content, Is.Empty);
                 Assert.That(inventory[1].IsEmpty, Is.True);
             });
+        }
+
+        [Test]
+        public void Move_EmptyOrigin_CallsOnMoveEventOnlyWithTarget()
+        {
+            var stackSize = this.random.Next(1, 10);
+            var inventory = this.containerFactory.EmptyContainer(2, stackSize);
+
+            var item = this.itemFactory.CreateRandom();
+            var originIndex = 0;
+            var targetIndex = 1;
+            inventory.AddAt(item, targetIndex, 1);
+
+            inventory.OnMove += (o, args) =>
+            {
+                Assert.That(args.Data, Has.Count.EqualTo(1));
+                Assert.Multiple(() =>
+                {
+                    var firstEvent = args.Data.FirstOrDefault();
+                    Assert.That(firstEvent.Item, Is.EqualTo(item));
+                    Assert.That(firstEvent.FromIndex, Is.EqualTo(targetIndex));
+                    Assert.That(firstEvent.ToIndex, Is.EqualTo(originIndex));
+                });
+            };
+            inventory.Move(originIndex, targetIndex);
         }
     }
 }
