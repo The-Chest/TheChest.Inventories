f51a34ebc2798c0f5b5339e4187059e575aa3e31
Franklin Bitencourt <Franklin.Bdantas@hotmail.com>
2025-09-19 17:16:04 -0300

    Adds unit tests to LazyStackInventoryTests.Replace

---
+++ tests/TheChest.Inventories.Tests/Containers/LazyStackInventory/LazyStackInventoryTests.Replace.cs
@@ -0,0 +1,158 @@
+﻿namespace TheChest.Inventories.Tests.Containers.LazyStackInventory
+{
+    public partial class LazyStackInventoryTests<T>
+    {
+        [TestCase(-1, 1)]
+        [TestCase(2000, 1)]
+        public void Replace_InvalidIndex_ThrowsArgumentOutOfRangeException(int index, int amount)
+        {
+            var item = this.itemFactory.CreateDefault();
+            var inventory = this.containerFactory.FullContainer(20, 1, item);
+
+            Assert.That(
+                () => inventory.Replace(item, index, amount),
+                Throws.TypeOf<ArgumentOutOfRangeException>().And.With.Message.Contains("index")
+            );
+        }
+
+        [TestCase(1, 0)]
+        [TestCase(0, -1)]
+        public void Replace_InvalidAmount_ThrowsArgumentOutOfRangeException(int index, int amount)
+        {
+            var item = this.itemFactory.CreateDefault();
+            var inventory = this.containerFactory.FullContainer(20, 1, item);
+
+            Assert.That(
+                () => inventory.Replace(item, index, amount),
+                Throws.TypeOf<ArgumentOutOfRangeException>().And.With.Message.Contains("amount")
+            );
+        }
+
+        [Test]
+        public void Replace_InvalidAmount_ThrowsArgumentOutOfRangeException()
+        {
+            var item = this.itemFactory.CreateDefault();
+            var inventory = this.containerFactory.FullContainer(20, 1, item);
+
+            Assert.That(
+                () => inventory.Replace(default! , 1, 1),
+                Throws.TypeOf<ArgumentNullException>().And.With.Message.Contains("item")
+            );
+        }
+
+        [Test]
+        public void Replace_SlotWithItems_ReplacesItemOnSlot()
+        {
+            var oldItem = this.itemFactory.CreateDefault();
+            var size = this.random.Next(2, 20);
+            var stackSize = this.random.Next(1, 10);
+            var inventory = this.containerFactory.FullContainer(size, stackSize, oldItem);
+
+            var newItem = this.itemFactory.CreateRandom();
+            var index = this.random.Next(0, size - 1);
+            inventory.Replace(newItem, index, stackSize);
+
+            Assert.That(inventory.GetSlots()![index].GetContent(), Is.EqualTo(newItem));
+        }
+
+        [Test]
+        public void Replace_SlotWithItems_CallsOnReplaceEventWithItems()
+        {
+            var oldItem = this.itemFactory.CreateDefault();
+            var size = this.random.Next(2, 20);
+            var stackSize = this.random.Next(1, 10);
+            var inventory = this.containerFactory.FullContainer(size, stackSize, oldItem);
+
+            var newItem = this.itemFactory.CreateRandom();
+            var index = this.random.Next(0, size - 1);
+
+            inventory.OnReplace += (sender, args) =>
+            {
+                Assert.That(args.Data, Has.Count.EqualTo(1));
+                var eventData = args.Data.First();
+                Assert.Multiple(() =>
+                {
+                    Assert.That(eventData.Index, Is.EqualTo(index));
+
+                    Assert.That(eventData.NewItem, Is.EqualTo(newItem));
+                    Assert.That(eventData.NewAmount, Is.EqualTo(stackSize));
+
+                    Assert.That(eventData.OldItem, Is.EqualTo(oldItem));
+                    Assert.That(eventData.OldAmount, Is.EqualTo(stackSize));
+                });
+            };
+            inventory.Replace(newItem, index, stackSize);
+        }
+
+        [Test]
+        public void Replace_SlotWithItems_ReturnsItemFromSlot()
+        {
+            var oldItem = this.itemFactory.CreateDefault();
+            var size = this.random.Next(2, 20);
+            var stackSize = this.random.Next(1, 10);
+            var inventory = this.containerFactory.FullContainer(size, stackSize, oldItem);
+
+            var newItem = this.itemFactory.CreateRandom();
+            var index = this.random.Next(0, size - 1);
+            var oldItems = inventory.Replace(newItem, index, stackSize);
+
+            Assert.That(oldItems, Has.Length.EqualTo(stackSize).And.All.EqualTo(oldItem));
+        }
+
+        [Test]
+        public void Replace_Empty_AddsItemToSlot()
+        {
+            var size = this.random.Next(2, 20);
+            var stackSize = this.random.Next(1, 10);
+            var inventory = this.containerFactory.EmptyContainer(size, stackSize);
+
+            var newItem = this.itemFactory.CreateRandom();
+            var index = this.random.Next(0, size - 1);
+            inventory.Replace(newItem, index, stackSize);
+
+            Assert.That(inventory.GetSlots()![index].GetContent(), Is.EqualTo(newItem));
+        }
+
+        [Test]
+        public void Replace_EmptySlot_ReturnsEmptyArray()
+        {
+            var size = this.random.Next(2, 20);
+            var stackSize = this.random.Next(1, 10);
+            var inventory = this.containerFactory.EmptyContainer(size, stackSize);
+
+            var newItem = this.itemFactory.CreateRandom();
+            var index = this.random.Next(0, size - 1);
+            var oldItems = inventory.Replace(newItem, index, stackSize);
+
+            Assert.That(oldItems, Is.Empty);
+        }
+
+        [Test]
+        public void Replace_EmptySlot_CallsOnReplaceEventWithNullOldItem()
+        {
+            var size = this.random.Next(2, 20);
+            var stackSize = this.random.Next(1, 10);
+            var inventory = this.containerFactory.EmptyContainer(size, stackSize);
+
+            var newItem = this.itemFactory.CreateRandom();
+            var index = this.random.Next(0, size - 1);
+
+            inventory.OnReplace += (sender, args) =>
+            {
+                Assert.That(args.Data, Has.Count.EqualTo(1));
+                var eventData = args.Data.First();
+                Assert.Multiple(() =>
+                {
+                    Assert.That(eventData.Index, Is.EqualTo(index));
+
+                    Assert.That(eventData.NewItem, Is.EqualTo(newItem));
+                    Assert.That(eventData.NewAmount, Is.EqualTo(stackSize));
+
+                    Assert.That(eventData.OldItem, Is.Null);
+                    Assert.That(eventData.OldAmount, Is.Zero);
+                });
+            };
+            inventory.Replace(newItem, index, stackSize);
+        }
+    }
+}
