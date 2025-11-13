b104c530fcc30d02440fe6007b39db508d6f0a7c
Franklin Bitencourt <Franklin.Bdantas@hotmail.com>
2025-11-13 19:54:52 -0300

    Separate methods from IInventoryExtensions to different files

---
+++ tests/TheChest.Inventories.Tests/Containers/Extensions/IStackInventoryExtensions.cs
@@ -0,0 +1,14 @@
+﻿using TheChest.Core.Slots.Interfaces;
+using TheChest.Inventories.Containers.Interfaces;
+
+namespace TheChest.Inventories.Tests.Containers.Extensions
+{
+    internal static class IStackInventoryExtensions
+    {
+        internal static IStackSlot<T>[]? GetSlots<T>(this IStackInventory<T> inventory)
+            => inventory.GetSlotsFieldOrProperty() as IStackSlot<T>[];
+
+        internal static T[]? GetItems<T>(this IStackInventory<T> inventory, int index)
+            => inventory.GetSlots()![index].GetContents();
+    }
+}
