52015a25696e2eb93d5a6bb46314dc0e132caf8d
Franklin Bitencourt <Franklin.Bdantas@hotmail.com>
2025-11-13 21:00:38 -0300

    Adds new global using config to unit tests

--- tests/TheChest.Inventories.Tests/Usings.cs
+++ tests/TheChest.Inventories.Tests/Usings.cs
@@ -3,4 +3,5 @@ global using TheChest.Inventories.Tests.Containers.Factories.Interfaces;
  
 global using TheChest.Core.Tests.Slots.Factories.Interfaces;
 global using TheChest.Inventories.Tests.Extensions;
- 
+
+global using TheChest.Inventories.Tests.Containers.Extensions;
