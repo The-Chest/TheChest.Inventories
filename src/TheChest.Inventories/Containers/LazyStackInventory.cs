258a2bd56082964b269a2bcd500ed7f0d357b79f
Franklin Bitencourt <Franklin.Bdantas@hotmail.com>
2026-01-18 02:34:15 -0300

    LazyStackInventory.Replace throws InvalidOperationException when item amount is bigger than slot size

--- src/TheChest.Inventories/Containers/LazyStackInventory.cs
+++ src/TheChest.Inventories/Containers/LazyStackInventory.cs
@@ -388,6 +388,7 @@ namespace TheChest.Inventories.Containers
         /// <inheritdoc/>
         /// <exception cref="ArgumentNullException">When <paramref name="item"/> is null</exception>
         /// <exception cref="ArgumentOutOfRangeException">When <paramref name="amount"/> is zero or smaller or <paramref name="index"/> is bigger than <see cref="StackContainer{T}.Size"/> or smaller than zero</exception>
+        /// <exception cref="InvalidOperationException">When <paramref name="amount"/> exceeds the stack size of the slot on <paramref name="index"/>.</exception>
         public virtual T[] Replace(T item, int index, int amount)
         {
             if (item is null)
@@ -398,6 +399,9 @@ namespace TheChest.Inventories.Containers
                 throw new ArgumentOutOfRangeException(nameof(index));
 
             var slot = this.slots[index];
+            if(!slot.CanReplace(item, amount))
+                throw new InvalidOperationException("The amount of items to replace exceeds the stack size of the slot.");
+
             var replacedItems = slot.Replace(item, amount);
 
             if (replacedItems.Length > 0)
