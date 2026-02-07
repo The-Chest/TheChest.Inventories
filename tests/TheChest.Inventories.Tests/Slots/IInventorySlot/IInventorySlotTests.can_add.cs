f2df81154921adb4c93f5d408c85bde6977f5808
Franklin Bitencourt <Franklin.Bdantas@hotmail.com>
2026-02-07 14:56:56 -0300

    Add validation methods to InventorySlot

---
+++ tests/TheChest.Inventories.Tests/Slots/IInventorySlot/IInventorySlotTests.can_add.cs
@@ -0,0 +1,37 @@
+﻿namespace TheChest.Inventories.Tests.Slots
+{
+    public partial class IInventorySlotTests<T>
+    {
+        [Test]
+        public void CanAdd_NullItem_ReturnsFalse()
+        {
+            var slot = this.slotFactory.EmptySlot();
+
+            var result = slot.CanAdd(default!);
+
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public void CanAdd_EmptySlot_ReturnsTrue()
+        {
+            var item = this.itemFactory.CreateDefault();
+            var slot = this.slotFactory.EmptySlot();
+
+            var result = slot.CanAdd(item);
+
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void CanAdd_FullSlot_ReturnsFalse()
+        {
+            var item = this.itemFactory.CreateDefault();
+            var slot = this.slotFactory.FullSlot(item);
+
+            var result = slot.CanAdd(item);
+
+            Assert.That(result, Is.False);
+        }
+    }
+}
