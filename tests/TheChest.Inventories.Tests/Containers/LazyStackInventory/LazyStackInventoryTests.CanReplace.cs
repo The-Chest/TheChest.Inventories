03cd5bf793d99939cfd6d90f1c1d526be0b530de
Franklin Bitencourt Dantas <franklin.bdantas@hotmail.com>
2026-02-14 17:58:08 -0300

    Fix Misleading Test Names in LazyStackInventoryTests.CanReplace

--- tests/TheChest.Inventories.Tests/Containers/LazyStackInventory/LazyStackInventoryTests.CanReplace.cs
+++ tests/TheChest.Inventories.Tests/Containers/LazyStackInventory/LazyStackInventoryTests.CanReplace.cs
@@ -29,7 +29,7 @@
         }
 
         [Test]
-        public void CanReplace_InvalidAmount_ThrowsArgumentOutOfRangeException()
+        public void CanReplace_NullItem_ThrowsArgumentNullException()
         {
             var item = this.itemFactory.CreateDefault();
             var inventory = this.containerFactory.FullContainer(20, 1, item);
@@ -80,9 +80,9 @@
 
             var newItem = this.itemFactory.CreateRandom();
             var index = this.random.Next(0, size - 1);
-            var oldItems = inventory.CanReplace(newItem, index, stackSize);
+            var canReplace = inventory.CanReplace(newItem, index, stackSize);
 
-            Assert.That(oldItems, Is.True);
+            Assert.That(canReplace, Is.True);
         }
     }
 }
