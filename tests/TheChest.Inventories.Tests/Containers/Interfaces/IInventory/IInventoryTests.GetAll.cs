6481e277b456e9a99ee6af7ee32a95a16372314c
Franklin Bitencourt <Franklin.Bdantas@hotmail.com>
2026-03-23 23:38:13 -0300

    Adds IInventoryTests.GetAll

---
+++ tests/TheChest.Inventories.Tests/Containers/Interfaces/IInventory/IInventoryTests.GetAll.cs
@@ -0,0 +1,32 @@
+﻿namespace TheChest.Inventories.Tests.Containers.Interfaces
+{
+    public partial class IInventoryTests<T>
+    {
+        [Test]
+        public void GetAll_FoundItems_ReturnsSearchingItem()
+        {
+            var size = this.random.Next(MIN_SIZE_TEST, MAX_SIZE_TEST);
+            var items = this.itemFactory.CreateMany(size / 2);
+            var sameItems = this.itemFactory.CreateManyRandom(size / 2);
+            var inventory = this.inventoryFactory.ShuffledItemsContainer(size, items.Concat(sameItems).ToArray());
+
+            var randomItem = sameItems[0];
+            var result = inventory.GetAll(randomItem);
+
+            Assert.That(result, Is.EqualTo(sameItems));
+        }
+
+        [Test]
+        public void GetAll_NotFoundItemm_ReturnsEmptyArray()
+        {
+            var size = this.random.Next(MIN_SIZE_TEST, MAX_SIZE_TEST);
+            var items = this.itemFactory.CreateMany(size);
+            var inventory = this.inventoryFactory.ShuffledItemsContainer(size, items);
+
+            var randomItem = this.itemFactory.CreateRandom();
+            var result = inventory.GetAll(randomItem);
+
+            Assert.That(result, Is.Empty);
+        }
+    }
+}
--- tests/TheChest.Inventories.Tests/Containers/Inventory/InventoryTests.GetAll.cs
+++ tests/TheChest.Inventories.Tests/Containers/Inventory/InventoryTests.GetAll.cs
@@ -1,4 +1,6 @@
-﻿namespace TheChest.Inventories.Tests.Containers
+﻿using TheChest.Tests.Common.Extensions;
+
+namespace TheChest.Inventories.Tests.Containers
 {
     public partial class InventoryTests<T>
     {
@@ -20,20 +22,6 @@
         }
 
         [Test]
-        public void GetAll_WithItems_ReturnsSearchingItem()
-        {
-            var size = this.random.Next(10, 20);
-            var items = this.itemFactory.CreateMany(size / 2);
-            var sameItems = this.itemFactory.CreateManyRandom(size / 2);
-            var inventory = this.containerFactory.ShuffledItemsContainer(size, items.Concat(sameItems).ToArray());
-
-            var randomItem = sameItems[0];
-            var result = inventory.GetAll(randomItem);
-        
-            Assert.That(result, Is.EqualTo(sameItems));
-        }
-
-        [Test]
         public void GetAll_WithItems_RemovesFromFoundSlots()
         {
             var size = this.random.Next(10, 20);
@@ -44,34 +32,21 @@
             var randomItem = items[index];
             inventory.GetAll(randomItem);
 
-            Assert.That(inventory.GetSlots()![index].IsEmpty, Is.True);
+            Assert.That(inventory.GetSlots<T>()![index].IsEmpty, Is.True);
         }
 
         [Test]
-        public void GetAll_NonExistingItem_ReturnsEmptyArray()
+        public void GetAll_NotFoundItem_DoesNotRemoveFromAnySlots()
         {
             var size = this.random.Next(10, 20);
             var items = this.itemFactory.CreateMany(size);
             var inventory = this.containerFactory.ShuffledItemsContainer(size, items);
-
-            var randomItem = this.itemFactory.CreateRandom();
-            var result = inventory.GetAll(randomItem);
-
-            Assert.That(result, Is.Empty);
-        }
-
-        [Test]
-        public void GetAll_NonExistingItem_DoesNotRemoveFromAnySlots()
-        {
-            var size = this.random.Next(10, 20);
-            var items = this.itemFactory.CreateMany(size);
-            var inventory = this.containerFactory.ShuffledItemsContainer(size, items);
-            var slots = inventory.GetSlots()?.ToArray();
+            var slots = inventory.GetSlots<T>()?.ToArray();
 
             var randomItem = this.itemFactory.CreateRandom();
             inventory.GetAll(randomItem);
 
-            Assert.That(inventory.GetSlots(), Is.EqualTo(slots));
+            Assert.That(inventory.GetSlots<T>(), Is.EqualTo(slots));
         }
 
         [Test]
