b344ad5844bde0a40cfb10009ae1469efcc26de1
Franklin Bitencourt <Franklin.Bdantas@hotmail.com>
2026-03-23 22:16:50 -0300

    Adds IInventoryTests.CanAddItemAt

---
+++ tests/TheChest.Inventories.Tests/Containers/Interfaces/IInventory/IInventoryTests.CanAddItemAt.cs
@@ -0,0 +1,64 @@
+﻿namespace TheChest.Inventories.Tests.Containers.Interfaces
+{
+    public partial class IInventoryTests<T>
+    {
+        [Test]
+        public void CanAddAt_EmptyInventory_ReturnsTrue()
+        {
+            var size = this.random.Next(MIN_SIZE_TEST, MAX_SIZE_TEST);
+            var inventory = this.inventoryFactory.EmptyContainer(size);
+
+            var item = this.itemFactory.CreateDefault();
+
+            var canAdd = inventory.CanAddAt(item, 0);
+
+            Assert.That(canAdd, Is.True);
+        }
+
+        [Test]
+        public void CanAddAt_SlotWithSameItem_ReturnsFalse()
+        {
+            var size = this.random.Next(MIN_SIZE_TEST, MAX_SIZE_TEST);
+            var inventory = this.inventoryFactory.EmptyContainer(size);
+
+            var randomIndex = this.random.Next(0, size);
+            var randomItem = this.itemFactory.CreateDefault();
+            inventory.AddAt(randomItem, randomIndex);
+
+            var canAdd = inventory.CanAddAt(randomItem, randomIndex);
+
+            Assert.That(canAdd, Is.False);
+        }
+
+        [Test]
+        public void CanAddAt_SlotWithDifferentItem_ReturnsFalse()
+        {
+            var size = this.random.Next(MIN_SIZE_TEST, MAX_SIZE_TEST);
+            var inventory = this.inventoryFactory.EmptyContainer(size);
+
+            var randomIndex = this.random.Next(0, size);
+            var randomItem = this.itemFactory.CreateRandom();
+            inventory.AddAt(randomItem, randomIndex);
+
+            var item = this.itemFactory.CreateDefault();
+            var canAdd = inventory.CanAddAt(item, randomIndex);
+
+            Assert.That(canAdd, Is.False);
+        }
+
+        [Test]
+        public void CanAddAt_FullInventory_ReturnsFalse()
+        {
+            var size = this.random.Next(MIN_SIZE_TEST, MAX_SIZE_TEST);
+            var item = this.itemFactory.CreateDefault();
+            var inventory = this.inventoryFactory.FullContainer(size, item);
+
+            var randomIndex = this.random.Next(0, size);
+            inventory.AddAt(item, randomIndex);
+
+            var canAdd = inventory.CanAddAt(item, randomIndex);
+
+            Assert.That(canAdd, Is.False);
+        }
+    }
+}
--- tests/TheChest.Inventories.Tests/Containers/Inventory/InventoryTests.CanAddItemAt.cs
+++ tests/TheChest.Inventories.Tests/Containers/Inventory/InventoryTests.CanAddItemAt.cs
@@ -22,47 +22,5 @@ namespace TheChest.Inventories.Tests.Containers
                 Throws.Exception.TypeOf<ArgumentOutOfRangeException>()
             );
         }
-
-        [Test]
-        public void CanAddAt_EmptyInventory_ReturnsTrue()
-        {
-            var inventory = this.containerFactory.EmptyContainer();
-
-            var item = this.itemFactory.CreateDefault();
-
-            var canAdd = inventory.CanAddAt(item, 0);
-
-            Assert.That(canAdd, Is.True);
-        }
-
-        [Test]
-        public void CanAddAt_FullSlotWithSameItem_ReturnsFalse()
-        {
-            var size = this.random.Next(10, 20);
-            var inventory = this.containerFactory.EmptyContainer(size);
-
-            var randomIndex = this.random.Next(0, size);
-            var randomItem = this.itemFactory.CreateDefault();
-            inventory.AddAt(randomItem, randomIndex);
-
-            var canAdd = inventory.CanAddAt(randomItem, randomIndex);
-            
-            Assert.That(canAdd, Is.False);
-        }
-
-        [Test]
-        public void CanAddAt_FullInventory_ReturnsFalse()
-        {
-            var size = this.random.Next(10, 20);
-            var item = this.itemFactory.CreateDefault();
-            var inventory = this.containerFactory.FullContainer(size, item);
-
-            var randomIndex = this.random.Next(0, size);
-            inventory.AddAt(item, randomIndex);
-
-            var canAdd = inventory.CanAddAt(item, randomIndex);
-
-            Assert.That(canAdd, Is.False);
-        }
     }
 }
