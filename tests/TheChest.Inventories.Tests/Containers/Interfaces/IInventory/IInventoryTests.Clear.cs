7540fd84b1eb82fa32eb414dce8e5a0d266d4de7
Franklin Bitencourt <Franklin.Bdantas@hotmail.com>
2026-03-23 22:43:30 -0300

    Adds IInventoryTests.CanMove

---
+++ tests/TheChest.Inventories.Tests/Containers/Interfaces/IInventory/IInventoryTests.Clear.cs
@@ -0,0 +1,29 @@
+﻿namespace TheChest.Inventories.Tests.Containers.Interfaces
+{
+    public partial class IInventoryTests<T>
+    {
+        [Test]
+        public void Clear_EmptyInventory_ReturnsEmptyArray()
+        {
+            var inventory = this.inventoryFactory.EmptyContainer();
+
+            var result = inventory.Clear();
+
+            Assert.That(result, Is.Empty);
+        }
+
+        [Test]
+        public void Clear_FullInventory_ReturnsEveryItemFromInventory()
+        {
+            var size = this.random.Next(MIN_SIZE_TEST, MAX_SIZE_TEST);
+            var items = this.itemFactory.CreateManyRandom(size / 2)
+                .Concat(this.itemFactory.CreateMany(size / 2))
+                .ToArray();
+            var inventory = this.inventoryFactory.ShuffledItemsContainer(size, items);
+
+            var result = inventory.Clear();
+
+            Assert.That(result, Is.EquivalentTo(items));
+        }
+    }
+}
--- tests/TheChest.Inventories.Tests/Containers/Inventory/InventoryTests.Clear.cs
+++ tests/TheChest.Inventories.Tests/Containers/Inventory/InventoryTests.Clear.cs
@@ -3,36 +3,13 @@
     public partial class InventoryTests<T>
     {
         [Test]
-        public void Clear_EmptyInventory_ReturnsEmptyArray()
-        {
-            var inventory = this.containerFactory.EmptyContainer();
-
-            var result = inventory.Clear();
-
-            Assert.That(result, Is.Empty);
-        }
-
-        [Test]
         public void Clear_EmptyInventory_DoesNotCallOnGetEvent()
         {
             var inventory = this.containerFactory.EmptyContainer();
+            
             inventory.OnGet += (sender, args) => Assert.Fail("OnGet should not be called if no item is found");
             
             inventory.Clear();
-        }
-
-        [Test]
-        public void Clear_FullInventory_ReturnsEveryItemFromInventory()
-        {
-            var size = this.random.Next(10, 20);
-            var items = this.itemFactory.CreateManyRandom(size / 2)
-                .Concat(this.itemFactory.CreateMany(size / 2))
-                .ToArray();
-            var inventory = this.containerFactory.ShuffledItemsContainer(size, items);
-
-            var result = inventory.Clear();
-
-            Assert.That(result, Is.EquivalentTo(items));
         }
 
         [Test]
