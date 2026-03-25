322fd98bf423a89540b44d6ef8f614d2fba0c651
Franklin Bitencourt <Franklin.Bdantas@hotmail.com>
2026-03-25 13:15:04 -0300

    Adds IInventoryTests.GetItems

---
+++ tests/TheChest.Inventories.Tests/Containers/Interfaces/IInventory/IInventoryTests.GetItems.cs
@@ -0,0 +1,45 @@
+﻿namespace TheChest.Inventories.Tests.Containers.Interfaces
+{
+    public partial class IInventoryTests<T>
+    {
+        [Test]
+        public void GetItems_ValidAmountNotFoundItem_ReturnsEmptyArray()
+        {
+            var size = this.random.Next(MIN_SIZE_TEST, MAX_SIZE_TEST);
+            var item = this.itemFactory.CreateDefault();
+            var inventory = this.inventoryFactory.FullContainer(size, item);
+
+            var amount = this.random.Next(1, size);
+            var result = inventory.Get(this.itemFactory.CreateRandom(), amount);
+
+            Assert.That(result, Has.Length.EqualTo(0));
+        }
+
+        [Test]
+        public void GetItems_ValidAmountFullInventory_ReturnsItemArrayWithAmountSize()
+        {
+            var size = this.random.Next(MIN_SIZE_TEST, MAX_SIZE_TEST);
+            var item = this.itemFactory.CreateDefault();
+            var inventory = this.inventoryFactory.FullContainer(size, item);
+
+            var amount = this.random.Next(1, size);
+            var result = inventory.Get(item, amount);
+
+            Assert.That(result, Has.Length.EqualTo(amount));
+        }
+
+        [Test]
+        public void GetItems_ValidAmountWithItems_ReturnsItemArrayWithMaxAvailable()
+        {
+            var size = this.random.Next(MIN_SIZE_TEST, MAX_SIZE_TEST);
+            var expectedAmount = this.random.Next(1, size / 2);
+            var items = this.itemFactory.CreateMany(expectedAmount);
+            var inventory = this.inventoryFactory.ShuffledItemsContainer(size, items);
+
+            var amount = this.random.Next(expectedAmount, size);
+            var result = inventory.Get(items[0], amount);
+
+            Assert.That(result, Has.Length.EqualTo(expectedAmount));
+        }
+    }
+}
--- tests/TheChest.Inventories.Tests/Containers/Inventory/InventoryTests.GetItems.cs
+++ tests/TheChest.Inventories.Tests/Containers/Inventory/InventoryTests.GetItems.cs
@@ -34,46 +34,6 @@
         }
 
         [Test]
-        public void GetItems_ValidAmountNotFoundItem_ReturnsEmptyArray()
-        {
-            var size = this.random.Next(10, 20);
-            var item = this.itemFactory.CreateDefault();
-            var inventory = this.containerFactory.FullContainer(size, item);
-
-            var amount = this.random.Next(1, size);
-            var result = inventory.Get(this.itemFactory.CreateRandom(), amount);
-
-            Assert.That(result, Has.Length.EqualTo(0));
-        }
-
-        [Test]
-        public void GetItems_ValidAmountFullInventory_ReturnsItemArrayWithAmountSize()
-        {
-            var size = this.random.Next(10, 20);
-            var item = this.itemFactory.CreateDefault();
-            var inventory = this.containerFactory.FullContainer(size, item);
-
-            var amount = this.random.Next(1, size);
-            var result = inventory.Get(item, amount);
-
-            Assert.That(result, Has.Length.EqualTo(amount));
-        }
-
-        [Test]
-        public void GetItems_ValidAmountWithItems_ReturnsItemArrayWithMaxAvailable()
-        {
-            var inventorySize = this.random.Next(10, 20);
-            var expectedAmount = this.random.Next(1, inventorySize / 2);
-            var items = this.itemFactory.CreateMany(expectedAmount);
-            var inventory = this.containerFactory.ShuffledItemsContainer(inventorySize, items);
-
-            var amount = this.random.Next(expectedAmount, inventorySize);
-            var result = inventory.Get(items[0], amount);
-        
-            Assert.That(result, Has.Length.EqualTo(expectedAmount));
-        }
-
-        [Test]
         public void GetItems_ExistingItemOnSlot_CallsOnGetEvent()
         {
             var inventorySize = this.random.Next(10, 20);
