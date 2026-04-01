2e72e2cdf6ee80bec65d6f11f5d54c7e2e6d7ba5
Franklin Bitencourt <Franklin.Bdantas@hotmail.com>
2026-04-01 10:00:38 -0300

    Fixes in ContainerExtensions

--- tests/TheChest.Inventories.Tests/Common/Extensions/Containers/IContainerExtensions.cs
+++ tests/TheChest.Inventories.Tests/Common/Extensions/Containers/IContainerExtensions.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Reflection;
-using TheChest.Core.Containers.Interfaces;
+﻿using TheChest.Core.Containers.Interfaces;
 using TheChest.Core.Slots.Interfaces;
 using TheChest.Tests.Common.Extensions;
 
@@ -11,36 +9,6 @@ namespace TheChest.Inventories.Tests.Common.Extensions.Containers
     /// </summary>
     public static class IContainerExtensions
     {
-        //TODO: add to TheChest.Tests.Common.Extensions
-        internal static Type GetContainerType(this Type containerType, Type interfaceType)
-        {
-            if (!interfaceType.IsInterface)
-                throw new ArgumentException($"'{interfaceType.FullName}' is not an interface.");
-
-            if (!interfaceType.IsAssignableFrom(containerType))
-                throw new ArgumentException($"Type '{containerType.FullName}' does not implement '{interfaceType.FullName}'.");
-
-            return containerType;
-        }
-
-        /// <summary>
-        /// Retrieves the value of the non-public or public instance field named "slots" from the specified container.
-        /// </summary>
-        /// <param name="container">The container object from which to retrieve the "slots" field value.</param>
-        /// <returns>The value of the "slots" field from the specified container.</returns>
-        /// <exception cref="InvalidOperationException">Thrown if the "slots" field does not exist on the container's type.</exception>
-        internal static object GetSlotsField(this object container)
-        {
-            var type = container.GetType();
-
-            var field = type.GetField(
-                "slots",
-                BindingFlags.Instance | BindingFlags.NonPublic |
-                BindingFlags.Public | BindingFlags.FlattenHierarchy
-            ) ?? throw new InvalidOperationException("Field 'slots' not found.");
-
-            return field.GetValue(container);
-        }
         /// <summary>
         /// Gets the array of slots contained in the specified container.
         /// </summary>
@@ -55,7 +23,7 @@ namespace TheChest.Inventories.Tests.Common.Extensions.Containers
         /// <param name="container">The container from which to retrieve the item.</param>
         /// <param name="index">Index of the slot to retrieve.</param>
         /// <returns>An <see cref="ISlot{T}"/> representing the slot at the specified index in the container, or <see langword="null"/> if the container does not contain slot data in the expected format or if the index is out of bounds.</returns>
-        public static ISlot<T> GetSlot<T>(this IContainer<T> container, int index) => container.GetSlots<T>()[index];
+        public static ISlot<T> GetSlot<T>(this IContainer<T> container, int index) => container.GetSlots()[index];
         /// <summary>
         /// Gets the content of the slot at the specified index from the container's array of slots.
         /// </summary>
@@ -63,6 +31,6 @@ namespace TheChest.Inventories.Tests.Common.Extensions.Containers
         /// <param name="container">The container from which to retrieve the item.</param>
         /// <param name="index">Index of the slot to retrieve.</param>
         /// <returns>An item of type <typeparamref name="T"/> representing the content of the slot at the specified index in the container, or <see langword="null"/> if the container does not contain slot data in the expected format, if the index is out of bounds, or if the slot at the specified index is empty.</returns>
-        public static T GetItem<T>(this IContainer<T> container, int index) => container.GetSlot<T>(index).GetContent<T>();
+        public static T GetItem<T>(this IContainer<T> container, int index) => container.GetSlot(index).GetContent();
     }
 }
--- tests/TheChest.Inventories.Tests/Common/Extensions/Containers/ILazyStackInventoryExtensions.cs
+++ tests/TheChest.Inventories.Tests/Common/Extensions/Containers/ILazyStackInventoryExtensions.cs
@@ -4,7 +4,7 @@ using TheChest.Core.Slots.Interfaces;
 
 namespace TheChest.Inventories.Tests.Common.Extensions.Containers
 {
-    public static class IStackContainerExtensions
+    public static class ILazyStackInventoryExtensions
     {
         /// <summary>
         /// Gets the array of slots contained in the specified container.
@@ -12,7 +12,7 @@ namespace TheChest.Inventories.Tests.Common.Extensions.Containers
         /// <typeparam name="T">The type of items stored in the slots.</typeparam>
         /// <param name="container">The container from which to retrieve the slots. </param>
         /// <returns>An array of <see cref="ISlot{T}"/> representing the slots in the container, or <see langword="null"/> if the container does not contain slot data in the expected format.</returns>
-        public static IStackSlot<T>[] GetSlots<T>(this IStackContainer<T> container) => container.GetSlotsField() as IStackSlot<T>[];
+        public static ILazyStackSlot<T>[] GetSlots<T>(this ILazyStackContainer<T> container) => container.GetSlotsField() as ILazyStackSlot<T>[];
         /// <summary>
         /// Gets the slot at the specified index from the container's array of slots.
         /// </summary>
@@ -20,7 +20,7 @@ namespace TheChest.Inventories.Tests.Common.Extensions.Containers
         /// <param name="container">The container from which to retrieve the item.</param>
         /// <param name="index">Index of the slot to retrieve.</param>
         /// <returns>An <see cref="ISlot{T}"/> representing the slot at the specified index in the container, or <see langword="null"/> if the container does not contain slot data in the expected format or if the index is out of bounds.</returns>
-        public static IStackSlot<T> GetSlot<T>(this IStackContainer<T> container, int index) => container.GetSlots()[index];
+        public static ILazyStackSlot<T> GetSlot<T>(this ILazyStackContainer<T> container, int index) => container.GetSlots()[index];
         /// <summary>
         /// Gets the content of the slot at the specified index from the container's array of slots.
         /// </summary>
@@ -28,6 +28,6 @@ namespace TheChest.Inventories.Tests.Common.Extensions.Containers
         /// <param name="container">The container from which to retrieve the item.</param>
         /// <param name="index">Index of the slot to retrieve.</param>
         /// <returns>An item of type <typeparamref name="T"/> representing the content of the slot at the specified index in the container, or <see langword="null"/> if the container does not contain slot data in the expected format, if the index is out of bounds, or if the slot at the specified index is empty.</returns>
-        public static T[] GetItems<T>(this IStackContainer<T> container, int index) => container.GetSlot(index).GetContents();
+        public static T GetItems<T>(this ILazyStackContainer<T> container, int index) => container.GetSlot(index).GetContent();
     }
 }
