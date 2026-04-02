ebd75fb815b6bf7fc5eb4d194150fddb83c4f6ce
Franklin Bitencourt <Franklin.Bdantas@hotmail.com>
2026-04-02 16:00:33 -0300

    Fixes in IStackInventory unit tests

--- tests/TheChest.Inventories.Tests/Containers/Interfaces/IStackInventory/IStackInventoryTests.AddItemsAt.cs
+++ tests/TheChest.Inventories.Tests/Containers/Interfaces/IStackInventory/IStackInventoryTests.AddItemsAt.cs
@@ -1,5 +1,5 @@
-﻿using TheChest.Inventories.Tests.Common.Extensions.Containers;
-using TheChest.Tests.Common.Extensions;
+﻿using TheChest.Tests.Common.Extensions.Containers;
+using TheChest.Tests.Common.Extensions.Slots;
 
 namespace TheChest.Inventories.Tests.Containers.Interfaces
 {
@@ -36,29 +36,6 @@ namespace TheChest.Inventories.Tests.Containers.Interfaces
         }
 
         [Test]
-        public void AddItemsAt_SlotWithSameItem_AddsToStack()
-        {
-            var size = this.random.Next(MIN_SIZE_TEST, MAX_SIZE_TEST);
-            var amount = this.random.Next(5, 10);
-            var slotItem = this.itemFactory.CreateDefault();
-            var inventory = this.inventoryFactory.FullContainer(size, amount, slotItem);
-
-            var index = this.random.Next(0, size);
-            inventory.Get(index, 2); 
-
-            var items = this.itemFactory.CreateMany(2);
-            inventory.AddAt(items, index);
-
-            Assert.Multiple(() =>
-            {
-                var slot = inventory.GetSlot(index);
-                Assert.That(slot!.IsFull, Is.True);
-                Assert.That(slot!.Amount, Is.EqualTo(amount));
-                Assert.That(slot!.GetContents()?.Reverse().Take(2), Is.EqualTo(items));
-            });
-        }
-
-        [Test]
         public void AddItemsAt_FullSlotSlotWithSameItem_ReturnsNotAddedItemsFromParam()
         {
             var size = this.random.Next(MIN_SIZE_TEST, MAX_SIZE_TEST);
@@ -71,36 +48,6 @@ namespace TheChest.Inventories.Tests.Containers.Interfaces
             var result = inventory.AddAt(items, index);
 
             Assert.That(result, Is.EqualTo(items));
-        }
-
-        [Test]
-        public void AddItemsAt_FullSlotWithSameItem_DoNotAddsToStack()
-        {
-            var size = this.random.Next(MIN_SIZE_TEST, MAX_SIZE_TEST);
-            var slotItem = this.itemFactory.CreateDefault();
-            var amount = this.random.Next(1, 10);
-            var inventory = this.inventoryFactory.FullContainer(size, amount, slotItem);
-
-            var index = this.random.Next(0, size);
-            var items = this.itemFactory.CreateMany(10);
-            inventory.AddAt(items, index);
-
-            Assert.That(inventory.GetItems(index), Has.No.AnyOf(items));
-        }
-
-        [Test]
-        public void AddItemsAt_FullSlotWithSameItem_DoesNotCallOnAddEvent()
-        {
-            var size = this.random.Next(MIN_SIZE_TEST, MAX_SIZE_TEST);
-            var slotItem = this.itemFactory.CreateDefault();
-            var amount = this.random.Next(1, 10);
-            var inventory = this.inventoryFactory.FullContainer(size, amount, slotItem);
-
-            inventory.OnAdd += (sender, e) => Assert.Fail("OnAdd event should not be called when is not possible to Add.");
-            
-            var items = this.itemFactory.CreateMany(10);
-            var index = this.random.Next(0, size);
-            inventory.AddAt(items, index);
         }
 
         [Test]
