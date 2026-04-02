a0645443eba2be45fd1aa7db872231e207dc95f5
Franklin Bitencourt <Franklin.Bdantas@hotmail.com>
2026-04-02 14:29:57 -0300

    Adds ILazyStackInventoryTests.Add unit tests

---
+++ tests/TheChest.Inventories.Tests/Containers/Interfaces/ILazyStackInventory/ILazyStackInventoryTests.AddAmount.cs
@@ -0,0 +1,47 @@
+﻿namespace TheChest.Inventories.Tests.Containers.Interfaces
+{
+    public partial class ILazyStackInventoryTests<T>
+    {
+        [Test]
+        public void Add_WithAmount_SuccessfullyAddedItems_ReturnsZero()
+        {
+            var inventory = this.inventoryFactory.EmptyContainer();
+            var item = this.itemFactory.CreateDefault();
+            var amount = this.random.Next(1, 5);
+
+            var result = inventory.Add(item, amount);
+
+            Assert.That(result, Is.Zero);
+        }
+
+        [Test]
+        public void Add_WithAmount_FullInventory_ReturnsRemainingAmount()
+        {
+            var size = this.random.Next(MIN_SIZE_TEST, MAX_SIZE_TEST);
+            var stackSize = this.random.Next(MIN_STACK_SIZE_TEST, MAX_STACK_SIZE_TEST);
+            var randomItem = this.itemFactory.CreateRandom();
+            var inventory = this.inventoryFactory.FullContainer(size, stackSize, randomItem);
+
+            var item = this.itemFactory.CreateDefault();
+            var amount = this.random.Next(1, 5);
+            var result = inventory.Add(item, amount);
+
+            Assert.That(result, Is.EqualTo(amount));
+        }
+
+        [Test]
+        public void Add_WithAmount_NotAllItemsBeAdded_ReturnsRemainingAmount()
+        {
+            var size = this.random.Next(MIN_SIZE_TEST, MAX_SIZE_TEST);
+            var stackSize = this.random.Next(MIN_STACK_SIZE_TEST, MAX_STACK_SIZE_TEST);
+            var randomItem = this.itemFactory.CreateManyRandom(size - 1);
+            var inventory = this.inventoryFactory.ShuffledItemsContainer(size, stackSize, randomItem);
+
+            var item = this.itemFactory.CreateDefault();
+            var amount = stackSize + this.random.Next(1, 5);
+            var result = inventory.Add(item, amount);
+
+            Assert.That(result, Is.EqualTo(amount - stackSize));
+        }
+    }
+}
---
+++ tests/TheChest.Inventories.Tests/Containers/Interfaces/ILazyStackInventory/ILazyStackInventoryTests.AddAt.cs
@@ -0,0 +1,48 @@
+﻿namespace TheChest.Inventories.Tests.Containers.Interfaces
+{
+    public partial class ILazyStackInventoryTests<T>
+    {
+        [Test]
+        public void AddAt_AllItemsSuccessfullyAdded_ReturnsZero()
+        {
+            var size = this.random.Next(MIN_SIZE_TEST, MAX_SIZE_TEST);
+            var stackSize = this.random.Next(MIN_STACK_SIZE_TEST, MAX_STACK_SIZE_TEST);
+            var inventory = this.inventoryFactory.EmptyContainer(size, stackSize);
+            var item = this.itemFactory.CreateDefault();
+
+            var notAddedCount = inventory.AddAt(item, 0, stackSize);
+            
+            Assert.That(notAddedCount, Is.Zero);
+        }
+
+        [Test]
+        public void AddAt_SlotWithDifferentItem_ReturnsNotAddedAmount()
+        {
+            var size = this.random.Next(MIN_SIZE_TEST, MAX_SIZE_TEST);
+            var stackSize = this.random.Next(MIN_STACK_SIZE_TEST, MAX_STACK_SIZE_TEST);
+            var randomItem = this.itemFactory.CreateManyRandom(size);
+            var inventory = this.inventoryFactory.ShuffledItemsContainer(size, stackSize, randomItem);
+
+            var item = this.itemFactory.CreateDefault();
+            var randomIndex = this.random.Next(0, size);
+            var result = inventory.AddAt(item, randomIndex, stackSize);
+
+            Assert.That(result, Is.EqualTo(stackSize));
+        }
+
+        [Test]
+        public void AddAt_AmountBiggerThanSlotSize_ReturnsAmount()
+        {
+            var size = this.random.Next(MIN_SIZE_TEST, MAX_SIZE_TEST);
+            var stackSize = this.random.Next(MIN_STACK_SIZE_TEST, MAX_STACK_SIZE_TEST);
+            var inventory = this.inventoryFactory.EmptyContainer(size, stackSize);
+
+            var item = this.itemFactory.CreateDefault();
+            var randomIndex = this.random.Next(0, size);
+            var amount = this.random.Next(1, 10) + stackSize;
+            var notAddedCount = inventory.AddAt(item, randomIndex, amount);
+
+            Assert.That(notAddedCount, Is.EqualTo(amount));
+        }
+    }
+}
