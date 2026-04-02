9e9059b6245c2ba88aec2ddb6bf585473a5be457
Franklin Bitencourt <Franklin.Bdantas@hotmail.com>
2026-04-02 15:11:30 -0300

    Adds interface and Implementation unit tests for LazyStackInventory.Clear

--- tests/TheChest.Inventories.Tests/Containers/LazyStackInventory/LazyStackInventoryTests.Clear.cs
+++ tests/TheChest.Inventories.Tests/Containers/LazyStackInventory/LazyStackInventoryTests.Clear.cs
@@ -1,24 +1,16 @@
 ﻿using NUnit.Framework.Internal;
 using TheChest.Core.Slots.Interfaces;
+using TheChest.Tests.Common.Extensions.Containers;
+using TheChest.Tests.Common.Extensions.Slots;
 
 namespace TheChest.Inventories.Tests.Containers.LazyStackInventory
 {
     public partial class LazyStackInventoryTests<T>
     {
         [Test]
-        public void Clear_EmptyInventory_ReturnsEmptyArray()
-        {
-            var inventory = this.containerFactory.EmptyContainer();
-
-            var result = inventory.Clear();
-
-            Assert.That(result, Is.Empty);
-        }
-
-        [Test]
         public void Clear_EmptyInventory_CallsOnGetEvent()
         {
-            var inventory = this.containerFactory.EmptyContainer();
+            var inventory = this.inventoryFactory.EmptyContainer();
             
             inventory.OnGet += (sender, args) => Assert.Fail("OnGet event should not be called for empty inventory");
             
@@ -26,25 +18,12 @@ namespace TheChest.Inventories.Tests.Containers.LazyStackInventory
         }
 
         [Test]
-        public void Clear_InventoryWithItems_ReturnsAllItems()
-        {
-            var size = this.random.Next(1, 20);
-            var stackSize = this.random.Next(1, 10);
-            var item = this.itemFactory.CreateDefault();
-            var inventory = this.containerFactory.FullContainer(size, stackSize, item);
-
-            var result = inventory.Clear();
-
-            Assert.That(result, Has.Length.EqualTo(size * stackSize).And.All.EqualTo(item));
-        }
-
-        [Test]
         public void Clear_InventoryWithItems_CallsOnGetEvent()
         {
-            var size = this.random.Next(1, 20);
-            var stackSize = this.random.Next(1, 10);
+            var size = this.random.Next(MIN_SIZE_TEST, MAX_SIZE_TEST);
+            var stackSize = this.random.Next(MIN_STACK_SIZE_TEST, MAX_STACK_SIZE_TEST);
             var item = this.itemFactory.CreateRandom();
-            var inventory = this.containerFactory.FullContainer(size, stackSize, item);
+            var inventory = this.inventoryFactory.FullContainer(size, stackSize, item);
 
             var raised = false;
             inventory.OnGet += (sender, args) => {
@@ -64,10 +43,10 @@ namespace TheChest.Inventories.Tests.Containers.LazyStackInventory
         [Test]
         public void Clear_InventoryWithItems_RemoveItemsFromAllSlots()
         {
-            var size = this.random.Next(1, 20);
-            var stackSize = this.random.Next(1, 10);
+            var size = this.random.Next(MIN_SIZE_TEST, MAX_SIZE_TEST);
+            var stackSize = this.random.Next(MIN_STACK_SIZE_TEST, MAX_STACK_SIZE_TEST);
             var item = this.itemFactory.CreateDefault();
-            var inventory = this.containerFactory.FullContainer(size, stackSize, item);
+            var inventory = this.inventoryFactory.FullContainer(size, stackSize, item);
 
             inventory.Clear();
 
