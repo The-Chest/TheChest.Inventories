ef8c934880dd53fec09da62d5bda3b8fbfd2237a
Franklin Bitencourt <Franklin.Bdantas@hotmail.com>
2026-04-02 16:13:30 -0300

    Adds interface and Implementation unit tests for LazyStackInventoryTests.GetCount

---
+++ tests/TheChest.Inventories.Tests/Containers/Interfaces/ILazyStackInventory/ILazyStackInventoryTests.GetCount.cs
@@ -0,0 +1,33 @@
+﻿namespace TheChest.Inventories.Tests.Containers.Interfaces
+{
+	public partial class ILazyStackInventoryTests<T>
+	{
+		[Test]
+		public void GetCount_NotFoundItem_ReturnsZero()
+		{
+			var size = this.random.Next(MIN_SIZE_TEST, MAX_SIZE_TEST);
+			var stackSize = this.random.Next(MIN_STACK_SIZE_TEST, MAX_STACK_SIZE_TEST);
+			var wrongItem = this.itemFactory.CreateRandom();
+			var inventory = this.inventoryFactory.ShuffledItemsContainer(size, stackSize, wrongItem);
+
+			var item = this.itemFactory.CreateDefault();
+			var count = inventory.GetCount(item);
+
+			Assert.That(count, Is.Zero);
+		}
+
+		[Test]
+		public void GetCount_ExistingItem_ReturnsCorrectCount()
+		{
+			var size = this.random.Next(MIN_SIZE_TEST, MAX_SIZE_TEST);
+			var stackSize = this.random.Next(MIN_STACK_SIZE_TEST, MAX_STACK_SIZE_TEST);
+			var item = this.itemFactory.CreateDefault();
+			var inventory = this.inventoryFactory.FullContainer(size, stackSize, item);
+
+			var count = size * stackSize;
+			var result = inventory.GetCount(item);
+
+			Assert.That(result, Is.EqualTo(count));
+		}
+	}
+}
--- tests/TheChest.Inventories.Tests/Containers/LazyStackInventory/LazyStackInventoryTests.GetCount.cs
+++ tests/TheChest.Inventories.Tests/Containers/LazyStackInventory/LazyStackInventoryTests.GetCount.cs
@@ -9,31 +9,5 @@
 
             Assert.Throws<ArgumentNullException>(() => inventory.GetCount(default!));
         }
-
-        [Test]
-        public void GetCount_NotFoundItem_ReturnsZero()
-        {
-            var wrongItem = this.itemFactory.CreateRandom();
-            var inventory = this.inventoryFactory.ShuffledItemsContainer(20, 10, wrongItem);
-
-            var item = this.itemFactory.CreateDefault();
-            var count = inventory.GetCount(item);
-
-            Assert.That(count, Is.Zero);
-        }
-
-        [Test]
-        public void GetCount_ExistingItem_ReturnsCorrectCount()
-        {
-            var size = this.random.Next(1, 20);
-            var stackSize = this.random.Next(1, 10);
-            var item = this.itemFactory.CreateDefault();
-            var inventory = this.inventoryFactory.FullContainer(size, stackSize, item);
-
-            var count = size * stackSize;
-            var result = inventory.GetCount(item);
-
-            Assert.That(result, Is.EqualTo(count));
-        }
     }
 }
