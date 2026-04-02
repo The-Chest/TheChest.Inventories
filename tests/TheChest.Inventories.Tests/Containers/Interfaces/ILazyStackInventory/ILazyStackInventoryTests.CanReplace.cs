9ddecc9a15adb2a8c2a916423552c215e5faea4b
Franklin Bitencourt <Franklin.Bdantas@hotmail.com>
2026-04-02 15:00:26 -0300

    Adds interface and Implementation unit tests for LazyStackInventory.CanReplace

---
+++ tests/TheChest.Inventories.Tests/Containers/Interfaces/ILazyStackInventory/ILazyStackInventoryTests.CanReplace.cs
@@ -0,0 +1,50 @@
+﻿namespace TheChest.Inventories.Tests.Containers.Interfaces
+{
+    public partial class ILazyStackInventoryTests<T>
+    {
+        [Test]
+        public void CanReplace_MoreItemsThanStackSize_ReturnsFalse()
+        {
+            var size = this.random.Next(MIN_SIZE_TEST, MAX_SIZE_TEST);
+            var stackSize = this.random.Next(MIN_STACK_SIZE_TEST, MAX_STACK_SIZE_TEST);
+            var oldItem = this.itemFactory.CreateDefault();
+            var inventory = this.inventoryFactory.FullContainer(size, stackSize, oldItem);
+
+            var newItem = this.itemFactory.CreateRandom();
+            var index = this.random.Next(0, size - 1);
+
+            var canReplace = inventory.CanReplace(newItem, index, stackSize + 1);
+
+            Assert.That(canReplace, Is.False);
+        }
+
+        [Test]
+        public void CanReplace_SlotWithItems_ReturnsTrue()
+        {
+            var size = this.random.Next(MIN_SIZE_TEST, MAX_SIZE_TEST);
+            var stackSize = this.random.Next(MIN_STACK_SIZE_TEST, MAX_STACK_SIZE_TEST);
+            var oldItem = this.itemFactory.CreateDefault();
+            var inventory = this.inventoryFactory.FullContainer(size, stackSize, oldItem);
+
+            var newItem = this.itemFactory.CreateRandom();
+            var index = this.random.Next(0, size - 1);
+            var canReplace = inventory.CanReplace(newItem, index, stackSize);
+
+            Assert.That(canReplace, Is.True);
+        }
+
+        [Test]
+        public void CanReplace_EmptySlot_ReturnsTrue()
+        {
+            var size = this.random.Next(MIN_SIZE_TEST, MAX_SIZE_TEST);
+            var stackSize = this.random.Next(MIN_STACK_SIZE_TEST, MAX_STACK_SIZE_TEST);
+            var inventory = this.inventoryFactory.EmptyContainer(size, stackSize);
+
+            var newItem = this.itemFactory.CreateRandom();
+            var index = this.random.Next(0, size - 1);
+            var canReplace = inventory.CanReplace(newItem, index, stackSize);
+
+            Assert.That(canReplace, Is.True);
+        }
+    }
+}
