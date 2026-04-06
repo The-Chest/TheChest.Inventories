d4077f377efa75045ca19be3f395869d5b5aa3b0
Franklin Bitencourt <Franklin.Bdantas@hotmail.com>
2026-04-06 14:47:11 -0300

    Adds IInventoryStackSlotTests.replace unit tests for implementation and interfaces

---
+++ tests/TheChest.Inventories.Tests/Slots/Interfaces/IInventoryStackSlot/IInventoryStackSlotTests.replace.cs
@@ -0,0 +1,45 @@
+﻿namespace TheChest.Inventories.Tests.Slots.Interfaces
+{
+    public partial class IInventoryStackSlotTests<T>
+    {
+        [Test]
+        public void Replace_EmptySlot_ReturnsEmptyArray()
+        {
+            var stackSize = this.random.Next(MIN_STACK_SIZE_TEST, MAX_STACK_SIZE_TEST);
+            var slot = this.slotFactory.EmptySlot(stackSize);
+
+            var items = this.itemFactory.CreateMany(stackSize);
+            var result = slot.Replace(items);
+
+            Assert.That(result, Is.Empty);
+        }
+
+        [Test]
+        public void Replace_ItemsDifferentFromSlot_ReturnsItemsFromSlot()
+        {
+            var stackSize = this.random.Next(MIN_STACK_SIZE_TEST, MAX_STACK_SIZE_TEST);
+            var items = this.itemFactory.CreateMany(stackSize);
+            var slot = this.slotFactory.WithItems(items);
+
+            var amount = this.random.Next(1, stackSize);
+            var replacingItems = this.itemFactory.CreateManyRandom(amount);
+            var result = slot.Replace(replacingItems);
+
+            Assert.That(result, Is.EqualTo(items));
+        }
+
+        [Test]
+        public void Replace_ItemsEqualToSlot_ReturnsItemsFromSlot()
+        {
+            var stackSize = this.random.Next(MIN_STACK_SIZE_TEST, MAX_STACK_SIZE_TEST);
+            var items = this.itemFactory.CreateMany(stackSize);
+            var slot = this.slotFactory.WithItems(items, stackSize);
+
+            var amount = this.random.Next(1, stackSize);
+            var replacingItems = this.itemFactory.CreateMany(amount);
+            var result = slot.Replace(replacingItems);
+
+            Assert.That(result, Is.EqualTo(items));
+        }
+    }
+}
