d3be30d9b19538b72bc2540ef269ee0ab7c94198
Franklin Bitencourt <Franklin.Bdantas@hotmail.com>
2026-04-06 10:07:12 -0300

    Adds IInventoryStackSlotTests.can_replace unit tests for implementation and interfaces

---
+++ tests/TheChest.Inventories.Tests/Slots/Interfaces/IInventoryStackSlot/IInventoryStackSlotTests.can_replace_item.cs
@@ -0,0 +1,27 @@
+﻿namespace TheChest.Inventories.Tests.Slots.Interfaces
+{
+    public partial class IInventoryStackSlotTests<T>
+    {
+        [Test]
+        public void CanReplaceItem_NullItem_ReturnsFalse()
+        {
+            var stackSize = this.random.Next(MIN_STACK_SIZE_TEST, MAX_STACK_SIZE_TEST);
+            var slot = this.slotFactory.EmptySlot(stackSize);
+
+            var item = (T)default!;
+
+            Assert.That(slot.CanReplace(item), Is.False);
+        }
+
+        [Test]
+        public void CanReplaceItem_NotNullItem_ReturnsTrue()
+        {
+            var stackSize = this.random.Next(MIN_STACK_SIZE_TEST, MAX_STACK_SIZE_TEST);
+            var slot = this.slotFactory.EmptySlot(stackSize);
+
+            var item = this.itemFactory.CreateDefault();
+            
+            Assert.That(slot.CanReplace(item), Is.True);
+        }
+    }
+}
