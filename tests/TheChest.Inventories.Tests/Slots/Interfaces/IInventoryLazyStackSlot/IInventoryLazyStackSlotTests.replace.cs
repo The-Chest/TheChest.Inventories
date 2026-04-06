e956e69d5b27c2543d2ef6ba0b6029b2ce13f8e8
Franklin Bitencourt <Franklin.Bdantas@hotmail.com>
2026-04-06 17:44:41 -0300

    Adds IInventoryLazyStackSlotTests.replace unit tests for implementation and interfaces

---
+++ tests/TheChest.Inventories.Tests/Slots/Interfaces/IInventoryLazyStackSlot/IInventoryLazyStackSlotTests.replace.cs
@@ -0,0 +1,36 @@
+﻿namespace TheChest.Inventories.Tests.Slots.Interfaces
+{
+    public partial class IInventoryLazyStackSlotTests<T> 
+    {
+        [Test]
+        public void Replace_EmptySlot_ReturnsEmptyArray()
+        {
+            var slot = this.slotFactory.EmptySlot();
+            var item = this.itemFactory.CreateDefault();
+            int amount = this.random.Next(1, 10);
+
+            var result = slot.Replace(item, amount);
+
+            Assert.That(result, Is.Empty);
+        }
+
+        [Test]
+        public void Replace_SlotWithItems_ReturnsItemFromSlot()
+        {
+            var initialItem = this.itemFactory.CreateDefault();
+            var maxAmount = this.random.Next(11, 20);
+            int initialAmount = this.random.Next(1, 10);
+            var slot = this.slotFactory.WithItem(initialItem, initialAmount, maxAmount);
+
+            var newItem = this.itemFactory.CreateRandom();
+            int newAmount = this.random.Next(1, 10);
+            var result = slot.Replace(newItem, newAmount);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(result, Has.Length.EqualTo(initialAmount));
+                Assert.That(result, Has.All.EqualTo(initialItem));
+            });
+        }
+    }
+}
