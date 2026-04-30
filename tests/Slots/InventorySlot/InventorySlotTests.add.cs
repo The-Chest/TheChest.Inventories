c53ba50ed40bdd003d833e48758eca4f216360b2
The-Chest-Library <the-chest.lib@hotmail.com>
2026-04-30 20:44:17 -0300

    Add InventorySlot Add/Get/Replace unit test coverage

--- tests/Slots/InventorySlot/InventorySlotTests.add.cs
+++ tests/Slots/InventorySlot/InventorySlotTests.add.cs
@@ -41,5 +41,24 @@ namespace TheChest.Inventories.Tests.Slots.InventorySlot
                 Throws.InvalidOperationException.With.Message.EqualTo("The slot is already full.")
             );
         }
+
+        [Test]
+        public void Add_NullItem_ThrowsArgumentNullException()
+        {
+            var slot = this.slotFactory.Empty();
+
+            Assert.That(() => slot.Add(default!), Throws.ArgumentNullException);
+        }
+
+        [Test]
+        public void Add_EmptySlot_ReturnsTrue_WhenAddingDirectly()
+        {
+            var slot = this.slotFactory.Empty();
+            var item = this.itemFactory.CreateDefault();
+
+            var result = slot.Add(item);
+
+            Assert.That(result, Is.True);
+        }
     }
 }
--- tests/Slots/InventorySlot/InventorySlotTests.replace.cs
+++ tests/Slots/InventorySlot/InventorySlotTests.replace.cs
@@ -26,5 +26,36 @@ namespace TheChest.Inventories.Tests.Slots.InventorySlot
 
             Assert.That(slot.GetContent(), Is.Not.Null.And.EqualTo(newItem));
         }
+
+        [Test]
+        public void Replace_ReplacingItemOnEmptySlot_ReturnsNull_WhenReplacingDirectly()
+        {
+            var slot = this.slotFactory.Empty();
+            var newItem = this.itemFactory.CreateDefault();
+
+            var result = slot.Replace(newItem);
+
+            Assert.That(result, Is.Null);
+        }
+
+        [Test]
+        public void Replace_ReplacingItemOnFullSlot_ReturnsPreviousItem()
+        {
+            var existingItem = this.itemFactory.CreateDefault();
+            var slot = this.slotFactory.Full(existingItem);
+            var newItem = this.itemFactory.CreateRandom();
+
+            var result = slot.Replace(newItem);
+
+            Assert.That(result, Is.EqualTo(existingItem));
+        }
+
+        [Test]
+        public void Replace_NullItem_ThrowsArgumentNullException()
+        {
+            var slot = this.slotFactory.Empty();
+
+            Assert.That(() => slot.Replace(default!), Throws.ArgumentNullException);
+        }
     }
 }
