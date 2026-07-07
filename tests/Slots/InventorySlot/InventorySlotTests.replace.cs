1faa88d9aebf493237e67ae4bc9242771a3b847e
Franklin Bitencourt <Franklin.Bdantas@hotmail.com>
2026-07-07 16:21:32 -0300

    Adds InventorySlot.Replace unit test for value types

--- tests/Slots/InventorySlot/InventorySlotTests.replace.cs
+++ tests/Slots/InventorySlot/InventorySlotTests.replace.cs
@@ -1,61 +1,123 @@
-﻿using TheChest.Tests.Common.Extensions.Slots;
+﻿using TheChest.Tests.Common.Attributes;
+using TheChest.Tests.Common.Extensions.Slots;
 
 namespace TheChest.Inventories.Tests.Slots.InventorySlot
 {
     public partial class InventorySlotTests<T>
     {
         [Test]
-        public void Replace_ReplacingItemOnEmptySlot_AddsItem()
+        [IgnoreIfValueType]
+        public void Replace_NullItem_ThrowsArgumentNullException()
         {
             var slot = this.slotFactory.Empty();
-            var newItem = this.itemFactory.CreateDefault();
 
-            slot.Replace(newItem);
-
-            Assert.That(slot.GetContent(), Is.Not.Null.And.EqualTo(newItem));
+            Assert.That(() => slot.Replace(default!), Throws.ArgumentNullException);
         }
 
         [Test]
-        public void Replace_ReplacingItemOnFullSlot_AddsItem()
+        [IgnoreIfReferenceType]
+        public void Replace_EmptySlot_DefaultItem_ReturnsDefault()
         {
-            var item = this.itemFactory.CreateDefault();
+            var slot = this.slotFactory.Empty();
+
+            var newItem = default(T);
+            var result = slot.Replace(newItem!);
+
+            Assert.That(result, Is.EqualTo(default(T)));
+        }
+
+        [Test]
+        [IgnoreIfReferenceType]
+        public void Replace_EmptySlot_DefaultItem_AddsDefault()
+        {
+            var slot = this.slotFactory.Empty();
+
+            var newItem = default(T);
+            slot.Replace(newItem!);
+            
+            Assert.Multiple(() =>
+            {
+                Assert.That(slot.GetContent(), Is.EqualTo(default(T)));
+                Assert.That(slot.IsFull, Is.True);
+            });
+        }
+
+        [Test]
+        [IgnoreIfReferenceType]
+        public void Replace_FullSlot_DefaultItem_ReturnsItem()
+        {
+            var item = this.itemFactory.CreateRandom();
             var slot = this.slotFactory.Full(item);
 
-            var newItem = this.itemFactory.CreateDefault();
-            slot.Replace(newItem);
+            var newItem = default(T);
+            var result = slot.Replace(newItem!);
 
-            Assert.That(slot.GetContent(), Is.Not.Null.And.EqualTo(newItem));
+            Assert.That(result, Is.EqualTo(item));
         }
 
         [Test]
-        public void Replace_ReplacingItemOnEmptySlot_ReturnsNull_WhenReplacingDirectly()
+        [IgnoreIfReferenceType]
+        public void Replace_FullSlot_DefaultItem_AddsDefault()
+        {
+            var item = this.itemFactory.CreateRandom();
+            var slot = this.slotFactory.Full(item);
+
+            var newItem = default(T);
+            slot.Replace(newItem!);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(slot.GetContent(), Is.EqualTo(default(T)));
+                Assert.That(slot.IsFull, Is.True);
+            });
+        }
+
+        [Test]
+        [IgnoreIfValueType]
+        public void Replace_EmptySlot_ReturnsNull()
         {
             var slot = this.slotFactory.Empty();
-            var newItem = this.itemFactory.CreateDefault();
 
+            var newItem = this.itemFactory.CreateDefault();
             var result = slot.Replace(newItem);
 
             Assert.That(result, Is.Null);
         }
 
         [Test]
-        public void Replace_ReplacingItemOnFullSlot_ReturnsPreviousItem()
-        {
-            var existingItem = this.itemFactory.CreateDefault();
-            var slot = this.slotFactory.Full(existingItem);
-            var newItem = this.itemFactory.CreateRandom();
-
-            var result = slot.Replace(newItem);
-
-            Assert.That(result, Is.EqualTo(existingItem));
-        }
-
-        [Test]
-        public void Replace_NullItem_ThrowsArgumentNullException()
+        [IgnoreIfValueType]
+        public void Replace_EmptySlot_AddsItem()
         {
             var slot = this.slotFactory.Empty();
 
-            Assert.That(() => slot.Replace(default!), Throws.ArgumentNullException);
+            var newItem = this.itemFactory.CreateDefault();
+            slot.Replace(newItem);
+
+            Assert.That(slot.GetContent(), Is.EqualTo(newItem));
+        }
+
+        [Test]
+        public void Replace_FullSlot_ReturnsItem()
+        {
+            var item = this.itemFactory.CreateRandom();
+            var slot = this.slotFactory.Full(item);
+
+            var newItem = this.itemFactory.CreateDefault();
+            var result = slot.Replace(newItem);
+
+            Assert.That(result, Is.EqualTo(item));
+        }
+
+        [Test]
+        public void Replace_FullSlot_AddsItem()
+        {
+            var item = this.itemFactory.CreateDefault();
+            var slot = this.slotFactory.Full(item);
+
+            var newItem = this.itemFactory.CreateRandom();
+            slot.Replace(newItem);
+
+            Assert.That(slot.GetContent(), Is.EqualTo(newItem));
         }
     }
 }
