19e02352d39f0e842f4c2652b97acd6440117665
Franklin Bitencourt <Franklin.Bdantas@hotmail.com>
2026-08-19 19:42:08 -0300

    Removes IInventoryTests

--- tests/Containers/Inventory/InventoryTests.cs
+++ tests/Containers/Inventory/InventoryTests.cs
@@ -1,28 +1,39 @@
-﻿using System;
-using System.Linq;
-using TheChest.Inventories.Containers;
+﻿using TheChest.Inventories.Containers;
 using TheChest.Inventories.Slots;
+using TheChest.Tests.Common;
+using TheChest.Tests.Common.Items.Interfaces;
+using TheChest.Tests.Common.Items.ReferenceType;
+using TheChest.Tests.Common.Items.ValueType;
 using TheChest.Inventories.Tests.Containers.Factories;
-using TheChest.Inventories.Tests.Containers.Interfaces;
 using TheChest.Inventories.Tests.Containers.Interfaces.Factories;
 using TheChest.Inventories.Tests.Slots.Factories;
 using TheChest.Inventories.Tests.Slots.Interfaces.Factories;
-using TheChest.Tests.Common.Items.ReferenceType;
-using TheChest.Tests.Common.Items.ValueType;
 
 namespace TheChest.Inventories.Tests.Containers.Inventory
 {
     [TestFixture(typeof(TestItem))]
     [TestFixture(typeof(TestEnumItem))]
     [TestFixture(typeof(TestStructItem))]
-    public partial class InventoryTests<T> : IInventoryTests<T>
+    public partial class InventoryTests<T> : BaseTest<T>
     {
+        protected readonly IInventoryFactory<T> inventoryFactory;
+        protected readonly IItemFactory<T> itemFactory;
+
+        protected const int MIN_SIZE_TEST = 10;
+        protected const int MAX_SIZE_TEST = 20;
+
         public InventoryTests() : base(configure =>
         {
             configure
                 .Register<IInventorySlotFactory<T>, InventorySlotFactory<InventorySlot<T>, T>>()
                 .Register<IInventoryFactory<T>, InventoryFactory<Inventory<T>, T>>();
-        }) { }
+        })
+        {
+            this.inventoryFactory = this.configurations.Resolve<IInventoryFactory<T>>();
+            this.itemFactory = this.configurations.Resolve<IItemFactory<T>>();
+        }
+
+        protected int GenerateRandomSize() => this.random.Next(MIN_SIZE_TEST, MAX_SIZE_TEST);
 
         protected (T First, T Second) CreateDistinctItems()
         {
